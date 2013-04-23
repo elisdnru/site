ee2fc918495183e8b9f7c2100cf3916b21f91345
ElisDN <mail@elisdn.ru>
2013-04-23 09:52:31 +0400

    Added parameters in the Follow module

--- protected/modules/follow/FollowModule.php
+++ protected/modules/follow/FollowModule.php
@@ -2,11 +2,6 @@
 
 class FollowModule extends DWebModule
 {
-    public static function system()
-    {
-        return true;
-    }
-
     public function getGroup()
     {
         return 'Прочее';
@@ -14,6 +9,54 @@ class FollowModule extends DWebModule
 
     public function getName()
     {
-        return 'Также я здесь';
+        return 'Следуйте за мной';
+    }
+
+    public function install()
+    {
+        Yii::app()->config->add(array(
+            array(
+                'param'=>'FOLLOW.TWITTER',
+                'label'=>'Логин Twitter',
+                'value'=>'',
+                'type'=>'string',
+                'default'=>'',
+            ),
+            array(
+                'param'=>'FOLLOW.LIVEJOURNAL',
+                'label'=>'Логин Livejournal',
+                'value'=>'',
+                'type'=>'string',
+                'default'=>'',
+            ),
+            array(
+                'param'=>'FOLLOW.GITHUB',
+                'label'=>'Логин Github',
+                'value'=>'',
+                'type'=>'string',
+                'default'=>'',
+            ),
+            array(
+                'param'=>'FOLLOW.FEEDBURNER',
+                'label'=>'Логин Feedburner',
+                'value'=>'',
+                'type'=>'string',
+                'default'=>'',
+            ),
+        ));
+
+        return parent::install();
+    }
+
+    public function uninstall()
+    {
+        Yii::app()->config->delete(array(
+            'FOLLOW.TWITTER',
+            'FOLLOW.LIVEJOURNAL',
+            'FOLLOW.GITHUB',
+            'FOLLOW.FEEDBURNER',
+        ));
+
+        return parent::uninstall();
     }
 }
--- protected/modules/follow/widgets/views/Follow.php
+++ protected/modules/follow/widgets/views/Follow.php
@@ -1,15 +1,17 @@
 <p class="follow center">
-    <a href="<?php echo Yii::app()->config->get('GENERAL.FEED_URL'); ?>" title="RSS"><span style="background-image:url('<?php echo $assetsUrl; ?>/32/feed.png');"></span></a>
-    <a href="http://twitter.com/elisdnru" title="Twitter"><span style="background-image:url('<?php echo $assetsUrl; ?>/32/twitter.png');"></span></a>
-    <a href="http://elisdn.livejournal.com" title="LiveJournal"><span style="background-image:url('<?php echo $assetsUrl; ?>/32/livejournal.png');"></span></a>
-    <a href="https://github.com/ElisDN" title="GitHub"><span style="background-image:url('<?php echo $assetsUrl; ?>/32/github.png');"></span></a>
+    <a rel="nofollow" href="<?php echo Yii::app()->config->get('GENERAL.FEED_URL'); ?>" title="RSS"><span style="background-image:url('<?php echo $assetsUrl; ?>/32/feed.png');"></span></a>
+    <?php if ($username = Yii::app()->config->get('FOLLOW.TWITTER')): ?><a rel="nofollow" href="http://twitter.com/<?php echo $username; ?>" title="Twitter"><span style="background-image:url('<?php echo $assetsUrl; ?>/32/twitter.png');"></span></a><?php endif; ?>
+    <?php if ($username = Yii::app()->config->get('FOLLOW.LIVEJOURNAL')): ?><a rel="nofollow" href="http://<?php echo $username; ?>.livejournal.com" title="LiveJournal"><span style="background-image:url('<?php echo $assetsUrl; ?>/32/livejournal.png');"></span></a><?php endif; ?>
+    <?php if ($username = Yii::app()->config->get('FOLLOW.GITHUB')): ?><a rel="nofollow" href="https://github.com/<?php echo $username; ?>" title="GitHub"><span style="background-image:url('<?php echo $assetsUrl; ?>/32/github.png');"></span></a><?php endif; ?>
 </p>
 
+<?php if ($username = Yii::app()->config->get('FOLLOW.FEEDBURNER')): ?>
 <div class="form">
-	<form action="http://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('http://feedburner.google.com/fb/a/mailverify?uri=elisdn', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true">
-		<input type="hidden" name="uri" value="elisdn" />
+	<form action="http://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('http://feedburner.google.com/fb/a/mailverify?uri=<?php echo $username; ?>', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true">
+		<input type="hidden" name="uri" value="<?php echo $username; ?>" />
 		<input type="hidden" name="loc" value="ru_RU" />
 		<div class="row center"><label>Введите ваш Email</label><input type="text" name="email" style="width:80%" /></div>
 		<div class="row buttons center"><input type="submit" value="Подписаться на новости" /></div>
 	</form>
 </div>
+<?php endif; ?>
