70aa4bac9425111754319b56b73200d852b7a0b8
ElisDN <mail@elisdn.ru>
2013-04-25 17:11:58 +0400

    Turned gridview ajax update off

--- protected/views/skins/CGridView.php
+++ protected/views/skins/CGridView.php
@@ -3,6 +3,7 @@
 return array(
     'default'=>array(
         'enableHistory' => false,
+        'ajaxUpdate'=>false,
         'cssFile' => Yii::app()->request->baseUrl . '/core/css/gridview.css',
         'summaryText' => '{start}&ndash;{end} из {count}',
     ),
