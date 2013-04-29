9a7f10cdef0ec2cc6d3c67bdd6a0cbe68320644f
ElisDN <mail@elisdn.ru>
2013-04-29 15:41:06 +0400

    Renamed tinymce directory

--- protected/modules/tinymce/widgets/TinyMCEWidget.php
+++ protected/modules/tinymce/widgets/TinyMCEWidget.php
@@ -9,10 +9,10 @@ class TinyMCEWidget extends DWidget
         $url = CHtml::asset(Yii::getPathOfAlias('tinymce.assets'));
 
         $cs->registerCssFile($url . '/tinymce.css');
-		$cs->registerScriptFile($url . '/tiny_mce/jquery.tinymce.js', CClientScript::POS_HEAD);
-		$cs->registerScriptFile($url . '/tiny_mce/plugins/tinybrowser/tb_tinymce.js.php', CClientScript::POS_END);
+		$cs->registerScriptFile($url . '/tinymce/jquery.tinymce.js', CClientScript::POS_HEAD);
+		$cs->registerScriptFile($url . '/tinymce/plugins/tinybrowser/tb_tinymce.js.php', CClientScript::POS_END);
 
-        $scriptUrl = $url . '/tiny_mce/tiny_mce.js';
+        $scriptUrl = $url . '/tinymce/tiny_mce.js';
         $styleUrl = Yii::app()->theme->baseUrl . '/css/tinymce.css';
 
         $this->render('tinymce', array(
