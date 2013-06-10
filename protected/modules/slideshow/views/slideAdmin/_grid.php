4abf48f4d49c7ea313a9bc4e82a4019459b99ad4
ElisDN <mail@elisdn.ru>
2013-06-10 17:09:28 +0400

    Refactored slides admin grid

--- protected/modules/slideshow/views/slideAdmin/_grid.php
+++ protected/modules/slideshow/views/slideAdmin/_grid.php
@@ -4,20 +4,26 @@
     'dataProvider'=>$model->search(),
     'filter'=>$model,
     'columns'=>array(
+
+		array(
+			'class'=>'DImageLinkColumn',
+			'value' => '$data->getImageThumbUrl()',
+			'width' => 250,
+			'htmlOptions'=>array('style'=>'width:250px;text-align:center'),
+		),
         array(
             'name'=>'sort',
             'htmlOptions'=>array('style'=>'width:50px;text-align:center'),
         ),
-        array(
-            'name' => 'title',
-            'value' => 'CHtml::link(CHtml::encode($data->title), Yii::app()->controller->createUrl("update", array("id"=>$data->id)))',
-            'type'  => 'html',
-        ),
-        array(
-            'name' => 'url',
-            'value' => 'CHtml::link(CHtml::encode($data->url), Yii::app()->controller->createUrl("update", array("id"=>$data->id)))',
-            'type'  => 'html',
-        ),
+		array(
+			'class'=>'DLinkColumn',
+			'name' => 'title',
+		),
+		array(
+			'class'=>'DLinkColumn',
+			'name' => 'url',
+			'htmlOptions'=>array('style'=>'text-align:center'),
+		),
         array(
             'class'=>'DButtonColumn',
             'template'=>'{view}',
