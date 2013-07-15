11197431704d87ed89ecd12280dd75996d9d0273
ElisDN <mail@elisdn.ru>
2013-07-15 17:52:10 +0400

    Fixed search fragment tag

--- protected/modules/search/components/SearchHighlighter.php
+++ protected/modules/search/components/SearchHighlighter.php
@@ -6,7 +6,7 @@ class SearchHighlighter
         {
             $pos = max(mb_stripos($text, $word, null, 'UTF-8') - 100, 0);
             $fragment = mb_substr($text, $pos, 200, 'UTF-8');
-            $highlighted = str_ireplace($word, '<span class="highlight">' . $word . '</span>', $fragment);
+            $highlighted = str_ireplace($word, '<mark>' . $word . '</mark>', $fragment);
         } else {
             $highlighted = mb_substr($text, 0, 200, 'UTF-8');
         }
