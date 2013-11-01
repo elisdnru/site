e0bb2ae3a6890ee31c4f1ecc1e66c45ec87f025f
ElisDN <mail@elisdn.ru>
2013-11-01 16:06:44 +0400

    Added nofollow into portfolio widget

--- protected/modules/portfolio/widgets/views/Portfolio.php
+++ protected/modules/portfolio/widgets/views/Portfolio.php
@@ -12,9 +12,9 @@
     <div class="portfolioSlider">
         <ul>
         <?php foreach ($items as $item):  ?>
-            <li><a href="<?php echo $item->url; ?>"><span class="thumb" style="background-image: url('<?php echo $item->getImageThumbUrl(190, 0); ?>')"><span><?php echo CHtml::encode($item->title); ?></span></span></a></li>
+            <li><a rel="nofollow" href="<?php echo $item->url; ?>"><span class="thumb" style="background-image: url('<?php echo $item->getImageThumbUrl(190, 0); ?>')"><span><?php echo CHtml::encode($item->title); ?></span></span></a></li>
         <?php endforeach; ?>
-            <li><a href="<?php echo Yii::app()->createUrl('/portfolio/default/index'); ?>"><span class="thumb" style="background-image: url('<?php echo Yii::app()->theme->baseUrl; ?>/images/portfolio.png')"></span></a></li>
+            <li><a rel="nofollow" href="<?php echo Yii::app()->createUrl('/portfolio/default/index'); ?>"><span class="thumb" style="background-image: url('<?php echo Yii::app()->theme->baseUrl; ?>/images/portfolio.png')"></span></a></li>
         </ul>
     </div>
 </div>
