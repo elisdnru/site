66995428c574c8d8d6bbcc73ebf6cc0ab18a998e
ElisDN <mail@elisdn.ru>
2013-11-11 23:51:47 +0400

    Added nofollow widget

---
+++ protected/modules/main/components/widgets/DNofollowWidget.php
@@ -0,0 +1,18 @@
+<?php
+
+class DNofollowWidget extends DWidget
+{
+	public function init()
+	{
+		ob_start();
+		ob_implicit_flush(false);
+	}
+
+	public function run()
+	{
+		$html = ob_get_clean();
+		$html = preg_replace('#<a\s([^>]+)\srel=".[^"]+"#i', '<a $1 ', $html);
+		$html = str_replace('<a ', '<a rel="nofollow" ', $html);
+		echo $html;
+	}
+} 
