62e8da514d5928641b7ac6a8d3f26e8b7b60e32e
ElisDN <mail@elisdn.ru>
2018-08-04 13:48:23 +0300

    Extracted tests directory

--- protected/tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$yiit=__DIR__.'/../../vendor/yiisoft/yii/framework/yiit.php';
-$config=__DIR__.'/../config/test.php';
-
-require_once($yiit);
-require_once(__DIR__.'/WebTestCase.php');
-
-Yii::createWebApplication($config);
---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+$yiit=__DIR__.'/../vendor/yiisoft/yii/framework/yiit.php';
+$config=__DIR__.'/../protected/config/test.php';
+
+require_once($yiit);
+require_once(__DIR__.'/WebTestCase.php');
+
+Yii::createWebApplication($config);
