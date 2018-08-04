ffada977a95867753e4d6e246991868ab582e779
ElisDN <mail@elisdn.ru>
2018-08-04 13:43:34 +0300

    Extracted env config

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../protected/config/env.php';
 
 $yiic=__DIR__.'/../vendor/yiisoft/yii/framework/yiic.php';
 $config=__DIR__.'/config/console.php';
