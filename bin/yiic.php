1bc5d1ca558e26d7e39ecb97994728423c10204a
ElisDN <mail@elisdn.ru>
2018-08-04 13:50:20 +0300

    Extracted bin directory

--- bin/yiic.php
+++ bin/yiic.php
@@ -4,6 +4,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../protected/config/env.php';
 
 $yiic=__DIR__.'/../vendor/yiisoft/yii/framework/yiic.php';
-$config=__DIR__.'/config/console.php';
+$config=__DIR__.'/../protected/config/console.php';
 
 require_once($yiic);
