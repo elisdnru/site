0f6f4039998d2a01148e42ba825c6f6be30ecb81
ElisDN <mail@elisdn.ru>
2018-10-22 12:07:43 +0300

    Removed shop mail views

--- src/views/email/order/client.php
+++
@@ -1,5 +0,0 @@
-<p style="font-family:arial; font-size:12px;">Ваш заказ на сайте http://<?php echo $_SERVER['SERVER_NAME']; ?></p>
-
-<p style="font-family:arial; font-size:12px;">Менеджер магазина скоро свяжется с Вами.</p>
-
-<?php echo Yii::app()->controller->renderPartial('application.views.email.order._contentfull', ['order' => $order], true); ?>
--- src/views/email/order/unpayed.php
+++
@@ -1,4 +0,0 @@
-<p style="font-family:arial; font-size:12px;">Здравсвуйте. Ваш заказ № <?php echo CHtml::encode($order->fullId); ?> на
-    сайте http://<?php echo $_SERVER['SERVER_NAME']; ?> отмечен неоплаченным.</p>
-
-<?php echo Yii::app()->controller->renderPartial('application.views.email.order._contentfull', ['order' => $order], true); ?>
