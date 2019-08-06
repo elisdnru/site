1644ae7a385ef2dc2718f6bc2271f8939d12b228
ElisDN <mail@elisdn.ru>
2019-08-06 19:46:29 +0300

    Fixed codestyle

--- src/modules/share/widgets/views/Share/default.php
+++ src/modules/share/widgets/views/Share/default.php
@@ -1,16 +1,17 @@
-<script>(function () {
-        if (window.pluso) if (typeof window.pluso.start == "function") return;
-        if (window.ifpluso == undefined) {
-            window.ifpluso = 1;
-            var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-            s.type = 'text/javascript';
-            s.charset = 'UTF-8';
-            s.async = true;
-            s.src = ('https:' == window.location.protocol ? 'https' : 'http') + '://share.pluso.ru/pluso-like.js';
-            var h = d[g]('body')[0];
-            h.appendChild(s);
-        }
-    })();
+<script>
+(function () {
+    if (window.pluso) if (typeof window.pluso.start == "function") return;
+    if (window.ifpluso == undefined) {
+        window.ifpluso = 1;
+        var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+        s.type = 'text/javascript';
+        s.charset = 'UTF-8';
+        s.async = true;
+        s.src = ('https:' == window.location.protocol ? 'https' : 'http') + '://share.pluso.ru/pluso-like.js';
+        var h = d[g]('body')[0];
+        h.appendChild(s);
+    }
+})();
 </script>
 <div class="pluso" style="margin-left: -4px"
      data-background="transparent"
