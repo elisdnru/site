5eed3d523b728917a29b68cb09cddcb690de1162
ElisDN <mail@elisdn.ru>
2019-09-15 11:48:18 +0300

    Inlined subscribe form block

--- src/modules/follow/widgets/views/Follow.php
+++ src/modules/follow/widgets/views/Follow.php
@@ -8,5 +8,18 @@ jQuery('#followMe').follow({
 /*]]>*/
 </script>
 
-<?php $this->widget(\app\modules\block\widgets\BlockWidget::class, ['id' => 'subscribe_sidebar']); ?>
+<div class="subscribe-form">
+    <form method="post" action="//elisdn.justclick.ru/subscribe/process/?rid%5B0%5D=blog" target="_blank"  onsubmit="return jc_chkscrfrm(this, false, false, false, false)">
+        <div class="row">
+            <input type="text" name="lead_name" placeholder="Ваше имя" />
+        </div>
+        <div class="row">
+            <input type="email" name="lead_email" placeholder="Ваш Email" />
+        </div>
+        <div class="row button">
+            <input type="submit" name="submit" value="Подписаться на статьи" />
+        </div>
+    </form>
+</div>
+<p class="what-there">Узнавайте о полезных статьях,<br />не пропускайте видеоуроки,<br />получайте бонусы.</p>
 
