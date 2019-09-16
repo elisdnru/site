ba48c6e4afbc368d6be6d44063a8e5b7ede3957e
ElisDN <mail@elisdn.ru>
2019-09-16 23:03:29 +0300

    Changed ulogin markup

--- src/modules/ulogin/widgets/views/uloginWidget.php
+++ src/modules/ulogin/widgets/views/uloginWidget.php
@@ -1,6 +1,5 @@
 <?php if (Yii::app()->user->isGuest) : ?>
-    <div id="uLogin" x-ulogin-params="display=<?php echo $display ?>;fields=<?php echo $fields ?>;providers=<?php echo $providers ?>;hidden=<?php echo $hidden ?>;redirect_uri=<?php echo urlencode($redirect) ?>"></div>
-
+    <div id="uLogin" data-ulogin="display=<?php echo $display ?>;fields=<?php echo $fields ?>;providers=<?php echo $providers ?>;hidden=<?php echo $hidden ?>;redirect_uri=<?php echo urlencode($redirect) ?>"></div>
 <?php else : ?>
     <?php
     $anchor = 'Выйти (' . Yii::app()->user->getName() . ')';
