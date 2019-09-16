051b64bcfb097d51e59ed6a9a8a5677367ffc96c
ElisDN <mail@elisdn.ru>
2019-09-16 16:58:31 +0300

    Fixed jQuery plugins

--- public/build/main.js
+++ public/build/main.js
@@ -52,119 +52,115 @@ jQuery.fn.doConfirm = function () {
 jQuery.fn.doConfirm2 = function () {
 }
 
-(function ($) {
-  $.fn.share = function (data) {
-    data = $.extend({
-      enc_url: '',
-      enc_title: '',
-      enc_desc: '',
-      enc_image: '',
-      tw_status: '',
-      assets_url: ''
-    }, data || {})
-    var blocks = [
-      {
-        name: 'ВКонтакте',
-        image: '/32/vkontakte.png',
-        url: 'http://vkontakte.ru/share.php?url=' + data.enc_url + '&;title=' + data.enc_title + '&description=' + data.enc_desc + '&image=' + data.enc_image,
-        width: 626,
-        height: 436
-      },
-      {
-        name: 'Twitter',
-        image: '/32/twitter.png',
-        url: 'http://twitter.com/home/?status=' + data.tw_status,
-        width: 640,
-        height: 480
-      },
-      {
-        name: 'Facebook',
-        image: '/32/facebook.png',
-        url: 'http://www.facebook.com/sharer.php?u=' + data.enc_url + '&t=' + data.enc_title,
-        width: 626,
-        height: 436
-      },
-      {
-        name: 'Одноклассники',
-        image: '/32/odnoklassniki.png',
-        url: 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl=' + data.enc_url,
-        width: 626,
-        height: 436
-      },
-      {
-        name: 'Яндекс',
-        image: '/32/yandex.png',
-        url: 'http://wow.ya.ru/posts_share_link.xml?url=' + data.enc_url + '&title=' + data.enc_title + '&body=' + data.enc_desc,
-        width: 626,
-        height: 436
-      },
-      {
-        name: 'Живой журнал',
-        image: '/32/livejournal.png',
-        url: 'http://www.livejournal.com/update.bml?event=' + data.enc_desc + '%3Cbr+%2F%3E' + data.enc_url + '&subject=' + data.enc_title + '&body=' + data.enc_desc,
-        width: 626,
-        height: 436
-      }
-    ]
-    return this.each(function () {
-      var links = []
-      var curr
-      var url
-      for (var i = 0; i < blocks.length; i++) {
-        curr = blocks[i]
-        url = curr.url.split('&').join('&amp;')
-        links.push('<a rel="nofollow" onclick="window.open(\'' + url + '\', \'' + curr.name + '\', \'width=' + curr.width + ', height=' + curr.height + '\'); return false;" href="' + url + '" title="' + curr.name + '"><span style="background-image:url(\'' + data.assets_url + curr.image + '\');"></span></a>')
-      }
-      $(this).html(links.join('\r\n'))
-    })
-  }
-})(jQuery)
+jQuery.fn.share = function (data) {
+  data = jQuery.extend({
+    enc_url: '',
+    enc_title: '',
+    enc_desc: '',
+    enc_image: '',
+    tw_status: '',
+    assets_url: ''
+  }, data || {})
+  var blocks = [
+    {
+      name: 'ВКонтакте',
+      image: '/32/vkontakte.png',
+      url: 'http://vkontakte.ru/share.php?url=' + data.enc_url + '&;title=' + data.enc_title + '&description=' + data.enc_desc + '&image=' + data.enc_image,
+      width: 626,
+      height: 436
+    },
+    {
+      name: 'Twitter',
+      image: '/32/twitter.png',
+      url: 'http://twitter.com/home/?status=' + data.tw_status,
+      width: 640,
+      height: 480
+    },
+    {
+      name: 'Facebook',
+      image: '/32/facebook.png',
+      url: 'http://www.facebook.com/sharer.php?u=' + data.enc_url + '&t=' + data.enc_title,
+      width: 626,
+      height: 436
+    },
+    {
+      name: 'Одноклассники',
+      image: '/32/odnoklassniki.png',
+      url: 'http://www.odnoklassniki.ru/dk?st.cmd=addShare&st.s=1&st._surl=' + data.enc_url,
+      width: 626,
+      height: 436
+    },
+    {
+      name: 'Яндекс',
+      image: '/32/yandex.png',
+      url: 'http://wow.ya.ru/posts_share_link.xml?url=' + data.enc_url + '&title=' + data.enc_title + '&body=' + data.enc_desc,
+      width: 626,
+      height: 436
+    },
+    {
+      name: 'Живой журнал',
+      image: '/32/livejournal.png',
+      url: 'http://www.livejournal.com/update.bml?event=' + data.enc_desc + '%3Cbr+%2F%3E' + data.enc_url + '&subject=' + data.enc_title + '&body=' + data.enc_desc,
+      width: 626,
+      height: 436
+    }
+  ]
+  return this.each(function () {
+    var links = []
+    var curr
+    var url
+    for (var i = 0; i < blocks.length; i++) {
+      curr = blocks[i]
+      url = curr.url.split('&').join('&amp;')
+      links.push('<a rel="nofollow" onclick="window.open(\'' + url + '\', \'' + curr.name + '\', \'width=' + curr.width + ', height=' + curr.height + '\'); return false;" href="' + url + '" title="' + curr.name + '"><span style="background-image:url(\'' + data.assets_url + curr.image + '\');"></span></a>')
+    }
+    jQuery(this).html(links.join('\r\n'))
+  })
+}
 
-(function ($) {
-  $.fn.follow = function (data) {
-    data = $.extend({
-      assets_url: ''
-    }, data || {})
-    var blocks = [
-      {
-        name: 'RSS',
-        image: '/32/feed.png',
-        url: 'https://feeds.feedburner.com/elisdn'
-      },
-      {
-        name: 'Twitter',
-        image: '/32/twitter.png',
-        url: 'https://twitter.com/elisdnru'
-      },
-      {
-        name: 'GitHub',
-        image: '/32/github.png',
-        url: 'https://github.com/ElisDN'
-      },
-      {
-        name: 'ВКонтакте',
-        image: '/32/vkontakte.png',
-        url: 'https://vk.com/elisdnru'
-      },
-      {
-        name: 'Facebook',
-        image: '/32/facebook.png',
-        url: 'https://www.facebook.com/elisdnru/'
-      },
-    ]
-    return this.each(function () {
-      var links = []
-      var curr
-      var url
-      for (var i = 0; i < blocks.length; i++) {
-        curr = blocks[i]
-        url = curr.url.split('&').join('&amp;')
-        links.push('<a rel="nofollow" href="' + url + '" title="' + curr.name + '"><span style="background-image:url(\'' + data.assets_url + curr.image + '\');"></span></a>')
-      }
-      $(this).html(links.join('\r\n'))
-    })
-  }
-})(jQuery)
+jQuery.fn.follow = function (data) {
+  data = jQuery.extend({
+    assets_url: ''
+  }, data || {})
+  var blocks = [
+    {
+      name: 'RSS',
+      image: '/32/feed.png',
+      url: 'https://feeds.feedburner.com/elisdn'
+    },
+    {
+      name: 'Twitter',
+      image: '/32/twitter.png',
+      url: 'https://twitter.com/elisdnru'
+    },
+    {
+      name: 'GitHub',
+      image: '/32/github.png',
+      url: 'https://github.com/ElisDN'
+    },
+    {
+      name: 'ВКонтакте',
+      image: '/32/vkontakte.png',
+      url: 'https://vk.com/elisdnru'
+    },
+    {
+      name: 'Facebook',
+      image: '/32/facebook.png',
+      url: 'https://www.facebook.com/elisdnru/'
+    },
+  ]
+  return this.each(function () {
+    var links = []
+    var curr
+    var url
+    for (var i = 0; i < blocks.length; i++) {
+      curr = blocks[i]
+      url = curr.url.split('&').join('&amp;')
+      links.push('<a rel="nofollow" href="' + url + '" title="' + curr.name + '"><span style="background-image:url(\'' + data.assets_url + curr.image + '\');"></span></a>')
+    }
+    jQuery(this).html(links.join('\r\n'))
+  })
+}
 
 function jc_setfrmfld (a) {
   var rnd = Math.floor(Math.random() * 10000)
--- public/js/follow.js
+++ public/js/follow.js
@@ -1,48 +1,47 @@
-(function ($) {
-  $.fn.follow = function (data) {
+jQuery.fn.follow = function (data) {
 
-    data = $.extend({
-      assets_url: ''
-    }, data || {})
+  data = jQuery.extend({
+    assets_url: ''
+  }, data || {})
 
-    var blocks = [
-      {
-        name: 'RSS',
-        image: '/32/feed.png',
-        url: 'https://feeds.feedburner.com/elisdn'
-      },
-      {
-        name: 'Twitter',
-        image: '/32/twitter.png',
-        url: 'https://twitter.com/elisdnru'
-      },
-      {
-        name: 'GitHub',
-        image: '/32/github.png',
-        url: 'https://github.com/ElisDN'
-      },
-      {
-        name: 'ВКонтакте',
-        image: '/32/vkontakte.png',
-        url: 'https://vk.com/elisdnru'
-      },
-      {
-        name: 'Facebook',
-        image: '/32/facebook.png',
-        url: 'https://www.facebook.com/elisdnru/'
-      },
-    ]
+  var blocks = [
+    {
+      name: 'RSS',
+      image: '/32/feed.png',
+      url: 'https://feeds.feedburner.com/elisdn'
+    },
+    {
+      name: 'Twitter',
+      image: '/32/twitter.png',
+      url: 'https://twitter.com/elisdnru'
+    },
+    {
+      name: 'GitHub',
+      image: '/32/github.png',
+      url: 'https://github.com/ElisDN'
+    },
+    {
+      name: 'ВКонтакте',
+      image: '/32/vkontakte.png',
+      url: 'https://vk.com/elisdnru'
+    },
+    {
+      name: 'Facebook',
+      image: '/32/facebook.png',
+      url: 'https://www.facebook.com/elisdnru/'
+    },
+  ]
 
-    return this.each(function () {
-      var links = []
-      var curr
-      var url
-      for (var i = 0; i < blocks.length; i++) {
-        curr = blocks[i]
-        url = curr.url.split('&').join('&amp;')
-        links.push('<a rel="nofollow" href="' + url + '" title="' + curr.name + '"><span style="background-image:url(\'' + data.assets_url + curr.image + '\');"></span></a>')
-      }
-      $(this).html(links.join('\r\n'))
-    })
-  }
-})(jQuery)
+  return this.each(function () {
+    var links = []
+    var curr
+    var url
+    for (var i = 0; i < blocks.length; i++) {
+      curr = blocks[i]
+      url = curr.url.split('&').join('&amp;')
+      links.push('<a rel="nofollow" href="' + url + '" title="' + curr.name + '"><span style="background-image:url(\'' + data.assets_url + curr.image + '\');"></span></a>')
+    }
+    jQuery(this).html(links.join('\r\n'))
+  })
+}
+
