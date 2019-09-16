4bbb5879d8787d99e892520f1a47f278b02057de
ElisDN <mail@elisdn.ru>
2019-09-16 13:50:51 +0300

    Changed assets codestyle

--- public/js/core-end.js
+++ public/js/core-end.js
@@ -1,77 +1,77 @@
-jQuery('.graytext').grayText();
+jQuery('.graytext').grayText()
 
-jQuery('.tdruller').tdRuller();
-jQuery('.confirm').doConfirm();
+jQuery('.tdruller').tdRuller()
+jQuery('.confirm').doConfirm()
 
-jQuery('.js_hide').hide();
+jQuery('.js_hide').hide()
 
 jQuery('span[data-href]').each(function () {
-	var span = jQuery(this);
-	span.replaceWith(jQuery('<a/>').attr('href', span.data('href')).html(span.html()));
-});
+  var span = jQuery(this)
+  span.replaceWith(jQuery('<a/>').attr('href', span.data('href')).html(span.html()))
+})
 
-jQuery(document).on('click', 'a.ajax_del', function(){
-    var t=jQuery(this);
-    if (!confirm(t.attr('title')+'?')) return false;
+jQuery(document).on('click', 'a.ajax_del', function () {
+  var t = jQuery(this)
+  if (!confirm(t.attr('title') + '?')) return false
 
-	jQuery.ajax({
-        type:'POST',
-        url:jQuery(this).attr('href'),
-        data:{'YII_CSRF_TOKEN': getCSRFToken()},
-        success:function(data) {
-            jQuery('#'+t.data('del')).hide(500);
-        },
-        error:function(XHR) {
-            alert(XHR.responseText);
-        }
-    });
+  jQuery.ajax({
+    type: 'POST',
+    url: jQuery(this).attr('href'),
+    data: { 'YII_CSRF_TOKEN': getCSRFToken() },
+    success: function (data) {
+      jQuery('#' + t.data('del')).hide(500)
+    },
+    error: function (XHR) {
+      alert(XHR.responseText)
+    }
+  })
 
-    return false;
-});
+  return false
+})
 
-jQuery(document).on('click', 'a.ajax_load', function(){
-    var t=jQuery(this);
-    if (!confirm(t.attr('title')+'?')) return false;
+jQuery(document).on('click', 'a.ajax_load', function () {
+  var t = jQuery(this)
+  if (!confirm(t.attr('title') + '?')) return false
 
-	jQuery.ajax({
-        type:'POST',
-        url:jQuery(this).attr('href'),
-        data:{'YII_CSRF_TOKEN': getCSRFToken()},
-        success:function(data) {
-            jQuery('#'+t.data('load')).html(data);
-        },
-        error:function(XHR) {
-            alert(XHR.responseText);
-        }
-    });
+  jQuery.ajax({
+    type: 'POST',
+    url: jQuery(this).attr('href'),
+    data: { 'YII_CSRF_TOKEN': getCSRFToken() },
+    success: function (data) {
+      jQuery('#' + t.data('load')).html(data)
+    },
+    error: function (XHR) {
+      alert(XHR.responseText)
+    }
+  })
 
-    return false;
-});
+  return false
+})
 
-jQuery(document).on('click', 'a.ajax_post', function(){
-    var t=jQuery(this);
-    if (!confirm(t.attr('title')+'?')) return false;
+jQuery(document).on('click', 'a.ajax_post', function () {
+  var t = jQuery(this)
+  if (!confirm(t.attr('title') + '?')) return false
 
-	jQuery.ajax({
-        type:'POST',
-        url:jQuery(this).attr('href'),
-        data:{'YII_CSRF_TOKEN': getCSRFToken()},
-        success:function(data) {
-            alert('Успешно');
-        },
-        error:function(XHR) {
-            alert(XHR.responseText);
-        }
-    });
+  jQuery.ajax({
+    type: 'POST',
+    url: jQuery(this).attr('href'),
+    data: { 'YII_CSRF_TOKEN': getCSRFToken() },
+    success: function (data) {
+      alert('Успешно')
+    },
+    error: function (XHR) {
+      alert(XHR.responseText)
+    }
+  })
 
-    return false;
-});
+  return false
+})
 
-jQuery('ul.collapsed li:has(li.active)').addClass('sup_active');
+jQuery('ul.collapsed li:has(li.active)').addClass('sup_active')
 
-jQuery('ul.collapsed > li:has(ul)').prepend($('<div>').addClass('collapse').prepend($('<span>').html('+')));
+jQuery('ul.collapsed > li:has(ul)').prepend($('<div>').addClass('collapse').prepend($('<span>').html('+')))
 
-jQuery('ul.collapsed > li .collapse').click(function(){
-    jQuery(this).parent().find('>ul').slideToggle(300);
-    return false;
-});
+jQuery('ul.collapsed > li .collapse').click(function () {
+  jQuery(this).parent().find('>ul').slideToggle(300)
+  return false
+})
--- public/js/jquery.plugins.js
+++ public/js/jquery.plugins.js
@@ -1,62 +1,62 @@
 jQuery.fn.grayText = function () {
 
-    return this.each(function () {
-        if ($(this).attr('type') == 'password') {
-            document.getElementById($(this).attr('id')).setAttribute('type', 'text');
-            $(this).data('type', 'password');
+  return this.each(function () {
+    if ($(this).attr('type') == 'password') {
+      document.getElementById($(this).attr('id')).setAttribute('type', 'text')
+      $(this).data('type', 'password')
+    }
+
+    if (!$(this).val()) $(this).val($(this).attr('title'))
+    if ($(this).val() == $(this).attr('title')) $(this).css('color', '#fff')
+
+    $(this).focus(function () {
+      if ($(this).val() == $(this).attr('title')) {
+        $(this).val('')
+        $(this).css('color', '#fff')
+        if ($(this).data('type') == 'password') {
+          document.getElementById($(this).attr('id')).setAttribute('type', 'password')
+          $(this).focus()
         }
-
-        if (!$(this).val()) $(this).val($(this).attr('title'));
-        if ($(this).val() == $(this).attr('title')) $(this).css('color', '#fff');
-
-        $(this).focus(function () {
-            if ($(this).val() == $(this).attr('title')) {
-                $(this).val('');
-                $(this).css('color', '#fff');
-                if ($(this).data('type') == 'password') {
-                    document.getElementById($(this).attr('id')).setAttribute('type', 'password');
-                    $(this).focus();
-                }
-            }
-        });
-
-        $(this).blur(function () {
-            if ($(this).val() == '') {
-                $(this).val($(this).attr('title'));
-                $(this).css('color', '#fff');
-                if ($(this).data('type') == 'password') {
-                    document.getElementById($(this).attr('id')).setAttribute('type', 'text');
-                }
-            }
-        })
+      }
     })
 
-};
+    $(this).blur(function () {
+      if ($(this).val() == '') {
+        $(this).val($(this).attr('title'))
+        $(this).css('color', '#fff')
+        if ($(this).data('type') == 'password') {
+          document.getElementById($(this).attr('id')).setAttribute('type', 'text')
+        }
+      }
+    })
+  })
+
+}
 
 jQuery.fn.tdRuller = function () {
-    return this.each(function () {
-        $(this).find('td').hover(function () {
-                $(this).parent().find('td').addClass('hover');
-            },
-            function () {
-                $(this).parent().find('td').removeClass('hover');
-            });
-        $(this).find('td').click(function () {
-            $(this).parent().parent().find('td').removeClass('select');
-            $(this).parent().find('td').addClass('select');
-        })
+  return this.each(function () {
+    $(this).find('td').hover(function () {
+        $(this).parent().find('td').addClass('hover')
+      },
+      function () {
+        $(this).parent().find('td').removeClass('hover')
+      })
+    $(this).find('td').click(function () {
+      $(this).parent().parent().find('td').removeClass('select')
+      $(this).parent().find('td').addClass('select')
     })
-};
+  })
+}
 
 jQuery.fn.doConfirm = function () {
-    return this.each(function () {
-        $(this).click(function () {
-            lbl = $(this).attr('title') ? $(this).attr('title') + '?' : 'Вы уверены?';
-            return window.confirm(lbl);
-        })
+  return this.each(function () {
+    $(this).click(function () {
+      lbl = $(this).attr('title') ? $(this).attr('title') + '?' : 'Вы уверены?'
+      return window.confirm(lbl)
     })
-};
+  })
+}
 
 jQuery.fn.doConfirm2 = function () {
 
-};
+}
