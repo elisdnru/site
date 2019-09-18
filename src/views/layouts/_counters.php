054ddcd7ae8989ed54bcd22cf003464ad0d5e0c3
Дмитрий Елисеев <mail@elisdn.ru>
2019-09-18 21:08:39 +0000

    Moved counters to bottom

--- src/views/layouts/_counters.php
+++ src/views/layouts/_counters.php
@@ -1,6 +1,5 @@
 <div>
-    <!--LiveInternet counter-->
-    <script type="text/javascript"><!--
+    <script>
     document.write('<a href=\'http://www.liveinternet.ru/click\' ' +
         'target=_blank><img src=\'//counter.yadro.ru/hit?t16.3;r' +
         escape(document.referrer) + ((typeof (screen) == 'undefined') ? '' :
@@ -9,32 +8,31 @@
         ';' + Math.random() +
         '\' alt=\'\' title=\'LiveInternet: показано число просмотров за 24' +
         ' часа, посетителей за 24 часа и за сегодня\' ' +
-        'border=\'0\' width=\'88\' height=\'31\'><\/a>')
-    //--></script>
-    <!--/LiveInternet-->
+        'border=\'0\' width=\'88\' height=\'31\'><\/a>');
+    </script>
 </div>
 
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
+<script>
+<?php ob_start(); ?>
 (function (m, e, t, r, i, k, a) {
     m[i] = m[i] || function () {(m[i].a = m[i].a || []).push(arguments)}
     m[i].l = 1 * new Date()
     k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-})
-(window, document, 'script', 'https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js', 'ym')
+})(window, document, 'script', 'https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js', 'ym');
 ym(18406216, 'init', {
     clickmap: false,
     trackLinks: true,
     accurateTrackBounce: true,
     webvisor: false
-})
+});
+<?php Yii::app()->clientScript->registerScript(__FILE__ . __LINE__, ob_get_clean(), CClientScript::POS_END); ?>
 </script>
 <noscript>
     <div><img src="https://mc.yandex.ru/watch/18406216" style="position:absolute; left:-9999px;" alt="" /></div>
 </noscript>
-<!-- /Yandex.Metrika counter -->
 
-<script type="text/javascript">
+<script>
+<?php ob_start(); ?>
 var _gaq = _gaq || []
 _gaq.push(['_setAccount', 'UA-3647' + '7750-1'])
 _gaq.push(['_trackPageview']);
@@ -45,9 +43,10 @@ _gaq.push(['_trackPageview']);
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'
     var s = document.getElementsByTagName('script')[0]
     s.parentNode.insertBefore(ga, s)
-})()
+})();
+<?php Yii::app()->clientScript->registerScript(__FILE__ . __LINE__, ob_get_clean(), CClientScript::POS_END); ?>
 </script>
 
-<script type="text/javascript">
+<script>
 (window.Image ? (new Image()) : document.createElement('img')).src = location.protocol + '//vk.com/rtrg?r=plhUXCWxYkaF/qlmUwYpFxoJuvC1rSUkMgO6hNlGX8VQOZPN8V6hnDR6ftdUkCPIXDsrr//qA*P0Fm0/Ww8meYxOx0uJgUaej2Xbg2c4tATTNuOdBnEIAqTmL*W7cPGPU0eEdbNdeg8EFUQWzMpr88hANhoO1lFTXC*7kXIezjM-'
 </script>
