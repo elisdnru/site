051df335fb7b4263b6b72f02baf303ba4c226c5c
ElisDN <mail@elisdn.ru>
2019-10-04 13:55:58 +0300

    Renamed calendar class

--- src/modules/blog/widgets/views/Calendar/calendar.php
+++ src/modules/blog/widgets/views/Calendar/calendar.php
@@ -1,4 +1,4 @@
-<div class="Calendar">
+<div class="calendar">
     <?php
     include_once(__DIR__ . '/generate_calendar.php');
     echo generate_calendar($year, $month, $days, $len, $url, 1, $pnc);
