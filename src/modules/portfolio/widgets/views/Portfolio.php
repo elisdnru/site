b6d148d8465d7e79c5dfc6872306263ff561e96e
ElisDN <mail@elisdn.ru>
2019-10-04 12:49:29 +0300

    Moved portfolio assets

--- src/modules/portfolio/widgets/views/Portfolio.php
+++ src/modules/portfolio/widgets/views/Portfolio.php
@@ -17,8 +17,10 @@ use app\modules\portfolio\models\PortfolioWork;
                     <a rel="nofollow" href="<?php echo $item->url; ?>"><span class="thumb" style="background-image: url('<?php echo $item->getImageThumbUrl(190, 0); ?>')"><span><?php echo CHtml::encode($item->title); ?></span></span></a>
                 </li>
             <?php endforeach; ?>
-            <li>
-                <a rel="nofollow" href="<?php echo Yii::app()->createUrl('/portfolio/default/index'); ?>"><span class="thumb" style="background-image: url('/images/portfolio.png')"></span></a>
+            <li class="more">
+                <a rel="nofollow" href="<?php echo Yii::app()->createUrl('/portfolio/default/index'); ?>">
+                    <span class="thumb"></span>
+                </a>
             </li>
         </ul>
     </div>
