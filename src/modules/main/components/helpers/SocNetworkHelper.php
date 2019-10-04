984e651874a72d01f2708d25a050e6f5dd95c4a9
ElisDN <mail@elisdn.ru>
2019-10-04 13:14:17 +0300

    Moved social icons

--- src/modules/main/components/helpers/SocNetworkHelper.php
+++ src/modules/main/components/helpers/SocNetworkHelper.php
@@ -4,27 +4,21 @@ namespace app\modules\main\components\helpers;
 
 class SocNetworkHelper
 {
-    const IMAGE_PATH = 'images/soc16/';
+    const IMAGE_PATH = 'images/socials/';
 
     public static function getIcon($network)
     {
         switch ($network) {
             case 'facebook':
-            case 'flickr':
             case 'google':
-            case 'lastfm':
             case 'linkedin':
             case 'liveid':
-            case 'livejournal':
             case 'mailru':
             case 'odnoklassniki':
             case 'openid':
-            case 'soundcloud':
             case 'steam':
             case 'twitter':
-            case 'vimeo':
             case 'vkontakte':
-            case 'webmoney':
             case 'yandex':
             case 'youtube':
                 $icon = $network . '.png';
