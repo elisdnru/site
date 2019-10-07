2da6dc646c48b2930ce5b9324507acd1e5609cfe
ElisDN <mail@elisdn.ru>
2019-10-07 15:26:04 +0300

    Moved follow script

--- src/components/widgets/views/Follow.php
+++ src/components/widgets/views/Follow.php
@@ -1,32 +1,5 @@
 <div id="follow" class="follow"></div>
 
-<script>
-<?php ob_start(); ?>
-
-(function() {
-    var blocks = [
-        {name: 'RSS', class: 'feed', url: 'https://feeds.feedburner.com/elisdn'},
-        {name: 'Twitter', class: 'twitter', url: 'https://twitter.com/elisdnru'},
-        {name: 'GitHub', class: 'github', url: 'https://github.com/ElisDN'},
-        {name: 'ВКонтакте', class: 'vkontakte', url: 'https://vk.com/elisdnru'},
-        {name: 'Facebook', class: 'facebook', url: 'https://www.facebook.com/elisdnru/'}
-    ];
-    var follow = document.querySelector('#follow');
-    blocks.forEach(function (block) {
-        var a = document.createElement('a');
-        a.rel = 'nofollow';
-        a.href = block.url;
-        a.title = block.name;
-        var span = document.createElement('span');
-        span.classList.add('follow-' + block.class);
-        a.appendChild(span);
-        follow.appendChild(a);
-    });
-})();
-
-<?php Yii::app()->clientScript->registerScript(__FILE__ . __LINE__, ob_get_clean(), CClientScript::POS_END); ?>
-</script>
-
 <div class="subscribe-form">
     <form method="post" action="//elisdn.justclick.ru/subscribe/process/?rid%5B0%5D=blog" target="_blank">
         <div class="row">
