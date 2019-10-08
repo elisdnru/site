3c82adc799fce60647d38fb0563ab741513558d9
ElisDN <mail@elisdn.ru>
2019-10-08 16:05:33 +0300

    Changes posts list markup

--- src/modules/blog/widgets/views/LastPosts/home.php
+++ src/modules/blog/widgets/views/LastPosts/home.php
@@ -13,18 +13,18 @@ foreach ($posts as $data) : ?>
             <div class="title"><a href="<?php echo $data->url; ?>"><?php echo CHtml::encode($data->title); ?></a></div>
             <!--noindex-->
             <div class="info">
-                <p class="date">
+                <div class="date">
                     <span class="enc-date" data-date="<?php echo DateHelper::normdate($data->date); ?>">&nbsp;</span>
-                </p>
+                </div>
                 <?php if ($data->category) : ?>
-                    <p class="category">
+                    <div class="category">
                         <span><a href="<?php echo $data->category->url; ?>"><?php echo CHtml::encode($data->category->title); ?></a></span>
-                    </p>
+                    </div>
                 <?php endif; ?>
-                <p class="tags"><span><?php echo implode(', ', $links); ?></span></p>
-                <p class="comments">
+                <div class="tags"><span><?php echo implode(', ', $links); ?></span></div>
+                <div class="comments">
                     <span><a href="<?php echo $data->url; ?>#comments"><?php echo $data->comments_count; ?></a></span>
-                </p>
+                </div>
             </div>
             <?php if ($data->image) : ?>
                 <?php $imageUrl = $data->getImageThumbUrl(250, 0); ?>
@@ -39,7 +39,7 @@ foreach ($posts as $data) : ?>
                     $properties['height'] = $data->image_height;
                 }
                 ?>
-                <p class="thumb">
+                <div class="thumb">
                     <a href="<?php echo $data->url; ?>">
                         <picture>
                             <source srcset="/images/lazy/blank.webp" data-srcset="<?= $imageUrl ?>.webp" type="image/webp">
@@ -47,12 +47,14 @@ foreach ($posts as $data) : ?>
                             <?php echo CHtml::image($imageUrl, '', $properties); ?>
                         </picture>
                     </a>
-                </p>
+                </div>
             <?php endif; ?>
             <!--/noindex-->
         </div>
         <div class="short"><?php echo trim($data->short_purified); ?></div>
-        <!--noindex--><p class="more"><a href="<?php echo $data->url; ?>">Читать далее</a></p><!--/noindex-->
+        <!--noindex-->
+        <div class="more"><a href="<?php echo $data->url; ?>">Читать далее</a></div>
+        <!--/noindex-->
     </div>
 
 <?php endforeach; ?>
