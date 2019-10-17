7f239c79770a66080e98c7f4b46c4b43b342122f
ElisDN <mail@elisdn.ru>
2019-10-17 16:15:34 +0300

    Changed email markup

--- src/views/email/remind.php
+++ src/views/email/remind.php
@@ -2,8 +2,8 @@
 /** @var $user \app\modules\user\models\User */
 use yii\helpers\Html;
 ?>
-<p>Запрошен сброс пароля на сайте <?= Yii::app()->request->getHostInfo() ?>.</p>
+<p>Запрошен сброс пароля на сайте <?= Yii::app()->request->getHostInfo() ?></p>
 
-<p>Временный пароль: <?php echo Html::encode($user->new_password); ?></p>
+<p>Временный пароль: <b><?php echo Html::encode($user->new_password); ?></b></p>
 
 <p>Войдите на сайт и смените пароль в кабинете.</p>
