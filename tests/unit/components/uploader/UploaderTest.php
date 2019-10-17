e1be30051d9716ffb3366d4c9670e988867142b6
ElisDN <mail@elisdn.ru>
2019-10-17 16:41:00 +0300

    Removed watermark code

--- tests/unit/components/uploader/UploaderTest.php
+++ tests/unit/components/uploader/UploaderTest.php
@@ -12,7 +12,6 @@ class UploaderTest extends CTestCase
     public function testThumbFileName(): void
     {
         $manager = new Uploader();
-        $manager->origFileSalt = 'adFxt0de';
 
         self::assertEquals('ab0cde_250x0.jpg', $manager->createThumbFileName('ab0cde.jpg', 250, 0));
         self::assertEquals('ab0cde_100x100.png', $manager->createThumbFileName('ab0cde.png', 100, 100));
