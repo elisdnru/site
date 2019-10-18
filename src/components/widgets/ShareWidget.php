e98fec08fe417828c83578cc3ec52a909046aa86
ElisDN <mail@elisdn.ru>
2019-10-18 12:48:15 +0300

    Removed share parameters autofilling

--- src/components/widgets/ShareWidget.php
+++ src/components/widgets/ShareWidget.php
@@ -15,29 +15,13 @@ class ShareWidget extends CWidget
 
     public function run(): void
     {
-        $this->initFields();
+        $host = Yii::app()->request->getHostInfo();
 
         $this->render('Share/' . $this->tpl, [
-            'url' => $this->url,
+            'url' => $this->url ?: $host . '/' . Yii::app()->request->getPathInfo(),
             'title' => $this->title,
             'description' => mb_substr(strip_tags($this->description), 0, 200, 'UTF-8') . '...',
-            'image' => $this->image,
+            'image' => $this->image ? $host . $this->image : '',
         ]);
-    }
-
-    protected function initFields(): void
-    {
-        if (!trim($this->url)) {
-            $this->url = Yii::app()->request->getHostInfo() . '/' . Yii::app()->request->getPathInfo();
-        }
-        if (!trim($this->title)) {
-            $this->title = Yii::app()->controller->pageTitle;
-        }
-        if (!trim($this->description)) {
-            $this->description = Yii::app()->controller->description;
-        }
-        if (trim($this->image)) {
-            $this->image = Yii::app()->request->getHostInfo() . $this->image;
-        }
     }
 }
