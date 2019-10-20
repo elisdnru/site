ee1e1ff2235223086cc0b12678a1332ae8453ba5
ElisDN <mail@elisdn.ru>
2019-10-20 14:26:26 +0300

    Fixed test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,15 +4,24 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+defined('YII_ENABLE_EXCEPTION_HANDLER') or define('YII_ENABLE_EXCEPTION_HANDLER',false);
+defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER',false);
+
 if (file_exists(__DIR__ . '/../.env')) {
     (new Dotenv())->load(__DIR__ . '/../.env');
 }
 
 require_once __DIR__.'/../config/env.php';
-$yiit=__DIR__.'/../vendor/yiisoft/yii/framework/yiit.php';
-$config=__DIR__.'/../config/test.php';
 
-require_once($yiit);
+require_once __DIR__ . '/../bootstrap.php';
+
+$yii2Config = require __DIR__ . '/../config/v2/test.php';
+new yii\web\Application($yii2Config);
+
+Yii::import('system.test.CTestCase');
+Yii::import('system.test.CDbTestCase');
+Yii::import('system.test.CWebTestCase');
+
 require_once(__DIR__.'/DbTestCase.php');
 
-Yii::createWebApplication($config);
+Yii::createWebApplication(__DIR__.'/../config/test.php');
