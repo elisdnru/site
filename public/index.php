542db25eaf96f338828a62a8afcb3c4e410024c3
ElisDN <mail@elisdn.ru>
2019-10-20 14:53:37 +0300

    Disabled Sentry init on dev

--- public/index.php
+++ public/index.php
@@ -9,7 +9,9 @@ if (file_exists(__DIR__ . '/../.env')) {
     (new Dotenv())->load(__DIR__ . '/../.env');
 }
 
-Sentry\init(['dsn' => getenv('SENTRY_DSN')]);
+if (getenv('SENTRY_DSN')) {
+    Sentry\init(['dsn' => getenv('SENTRY_DSN')]);
+}
 
 require_once __DIR__ . '/../config/env.php';
 
