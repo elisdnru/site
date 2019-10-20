c11da760b266d121a410dade02a90dc5bb6be8d1
ElisDN <mail@elisdn.ru>
2019-10-20 14:45:43 +0300

    Changed output buffering area

--- public/index.php
+++ public/index.php
@@ -18,9 +18,10 @@ require_once __DIR__ . '/../bootstrap.php';
 $yii2Config = require __DIR__ . '/../config/v2/web.php';
 new yii\web\Application($yii2Config);
 
-ob_start();
 $app = Yii::createWebApplication(__DIR__ . '/../config/web.php');
 $app->attachBehavior('SentryBehavior', new SentryBehavior(!(bool)getenv('APP_DEBUG')));
+
+ob_start();
 $app->run();
 $html = ob_get_clean();
 
