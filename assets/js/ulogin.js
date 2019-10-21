69e08050cc80ecb4c5c7c17d918ae1b0cc049175
ElisDN <mail@elisdn.ru>
2019-10-21 14:09:57 +0300

    Moved uLogin to observer

--- assets/js/ulogin.js
+++ assets/js/ulogin.js
@@ -1,16 +1,35 @@
-(function() {
+(function () {
   var widget = document.querySelector('#uLogin');
-  if (widget) {
-    var active = false;
-    window.addEventListener('scroll', function() {
-      if (active || window.pageYOffset + window.innerHeight < widget.offsetTop - 200) {
-        return;
+  if (!widget) {
+    return;
+  }
+
+  var config = {
+    rootMargin: '50px 0px',
+    threshold: 0.01
+  };
+
+  var loadWidget = function () {
+    var s = document.createElement('script');
+    s.src = '//ulogin.ru/js/ulogin.js';
+    s.async = true;
+    document.querySelector('body').appendChild(s);
+  }
+
+  var onChange = function (changes, observer) {
+    changes.forEach(function (change) {
+      if (change.intersectionRatio > 0) {
+        loadWidget();
+        observer.unobserve(change.target);
       }
-      var s = document.createElement('script');
-      s.src = '//ulogin.ru/js/ulogin.js';
-      s.async = true;
-      document.querySelector('body').appendChild(s);
-      active = true;
     });
+  }
+
+  if (window.IntersectionObserver) {
+    var observer = new IntersectionObserver(onChange, config);
+    observer.observe(widget);
+  } else {
+    console.log('%cIntersection Observers not supported');
+    loadWidget();
   }
 })();
