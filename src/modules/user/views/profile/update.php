17d149fcb2f04831dbb754e5626683947b5cef1a
ElisDN <mail@elisdn.ru>
2019-10-21 16:20:21 +0300

    Updated user profile form

--- src/modules/user/views/profile/update.php
+++ src/modules/user/views/profile/update.php
@@ -2,6 +2,8 @@
 
 use app\components\widgets\Portlet;
 use app\modules\user\models\Access;
+use yii\helpers\Html;
+
 /** @var $form CActiveForm */
 /** @var $model \app\modules\user\models\User */
 $this->layout = '/layouts/user';
@@ -18,84 +20,88 @@ if (Yii::app()->user->checkAccess(Access::ROLE_CONTROL)) {
 <?php $this->beginWidget(Portlet::class, ['title' => 'Редактировать профиль']); ?>
 
 <div class="form">
-    <?php $form = $this->beginWidget(CActiveForm::class, [
-        'id' => 'settings-form',
-        'enableClientValidation' => true,
-        'clientOptions' => [
-            'validateOnSubmit' => true,
-        ],
-        'htmlOptions' => ['enctype' => 'multipart/form-data']
-    ]); ?>
 
-    <?php echo $form->errorSummary($model); ?>
+    <form action="?" method="post" enctype="multipart/form-data">
 
-    <div>
-        <h4>Пользователь</h4>
+        <?= Html::hiddenInput(Yii::app()->request->csrfTokenName, Yii::app()->request->getCsrfToken()) ?>
+
+        <?= Html::errorSummary($model, ['class' => 'errorSummary']) ?>
+
+        <div>
+            <h4>Пользователь</h4>
+
+            <hr />
+
+            <div class="row<?= $model->hasErrors('lastname') ? ' error' : '' ?>">
+                <?= Html::activeLabel($model, 'lastname') ?><br />
+                <?= Html::activeTextInput($model, 'lastname', ['size' => 40, 'maxlength' => 255]) ?><br />
+                <?= Html::error($model, 'lastname', ['class' => 'errorMessage']) ?>
+            </div>
+
+            <div class="row<?= $model->hasErrors('name') ? ' error' : '' ?>">
+                <?= Html::activeLabel($model, 'name') ?><br />
+                <?= Html::activeTextInput($model, 'name', ['size' => 40, 'maxlength' => 255]) ?><br />
+                <?= Html::error($model, 'name', ['class' => 'errorMessage']) ?>
+            </div>
+
+            <div class="row<?= $model->hasErrors('middlename') ? ' error' : '' ?>">
+                <?= Html::activeLabel($model, 'middlename') ?><br />
+                <?= Html::activeTextInput($model, 'middlename', ['size' => 40, 'maxlength' => 255]) ?><br />
+                <?= Html::error($model, 'middlename', ['class' => 'errorMessage']) ?>
+            </div>
+
+            <div class="row<?= $model->hasErrors('site') ? ' error' : '' ?>">
+                <?= Html::activeLabel($model, 'site') ?><br />
+                <?= Html::activeTextInput($model, 'site', ['type' => 'url', 'size' => 40, 'maxlength' => 255]) ?><br />
+                <?= Html::error($model, 'site', ['class' => 'errorMessage']) ?>
+            </div>
+
+            <hr />
+
+            <h4>Аватар</h4>
+
+            <div class="row">
+                <p style="float:right"><img src="<?php echo $model->getAvatarUrl(); ?>" alt="" width="50" height="50"></p>
+                <?= Html::activeLabel($model, 'avatar') ?><br />
+                <?= Html::activeFileInput($model, 'avatar', ['size' => 30]) ?><label>
+                    <?= Html::error($model, 'avatar') ?>
+            </div>
+
+            <div class="row">
+                <?= Html::activeCheckbox($model, 'del_avatar') ?>
+            </div>
 
         <hr />
-        <div class="row">
-            <?php echo $form->labelEx($model, 'lastname'); ?><br />
-            <?php echo $form->textField($model, 'lastname', ['size' => 40, 'maxlength' => 255]); ?>
-            <?php echo $form->error($model, 'lastname'); ?>
-        </div>
 
-        <div class="row">
-            <?php echo $form->labelEx($model, 'name'); ?><br />
-            <?php echo $form->textField($model, 'name', ['size' => 40, 'maxlength' => 255]); ?>
-            <?php echo $form->error($model, 'name'); ?>
-        </div>
-
-        <div class="row">
-            <?php echo $form->labelEx($model, 'middlename'); ?><br />
-            <?php echo $form->textField($model, 'middlename', ['size' => 40, 'maxlength' => 255]); ?>
-            <?php echo $form->error($model, 'middlename'); ?>
-        </div>
-
-        <div class="row">
-            <?php echo $form->labelEx($model, 'site'); ?><br />
-            <?php echo $form->textField($model, 'site', ['size' => 40, 'maxlength' => 255]); ?>
-            <?php echo $form->error($model, 'site'); ?>
-        </div>
-
-        <hr />
-        <h4>Аватар</h4>
-        <div class="row">
-            <p style="float:right"><img src="<?php echo $model->avatarUrl; ?>" alt="" width="50" height="50"></p>
-            <?php echo $form->labelEx($model, 'avatar'); ?><br />
-            <?php echo $form->fileField($model, 'avatar', ['size' => 30]); ?><label>
-                <?php echo $form->error($model, 'avatar'); ?>
-        </div>
-
-        <div class="row">
-            <?php echo $form->checkBox($model, 'del_avatar'); ?><?php echo $form->labelEx($model, 'del_avatar'); ?>
-        </div>
-
-        <hr />
         <h4>Смена пароля</h4>
-        <div class="row">
-            <?php echo $form->labelEx($model, 'old_password'); ?> &nbsp;
+
+        <div class="row<?= $model->hasErrors('old_password') ? ' error' : '' ?>">
+            <?= Html::activeLabel($model, 'old_password') ?> &nbsp;
             (<a target="_blank" href="<?php echo $this->createUrl('/user/default/remind'); ?>">получить</a>)<br />
-            <?php echo $form->passwordField($model, 'old_password', ['size' => 40, 'maxlength' => 255]); ?>
-            <?php echo $form->error($model, 'old_password'); ?>
+            <?= Html::activePasswordInput($model, 'old_password', ['size' => 40, 'maxlength' => 255]) ?><br />
+            <?= Html::error($model, 'old_password', ['class' => 'errorMessage']) ?>
         </div>
-        <div class="row">
-            <?php echo $form->labelEx($model, 'new_password'); ?><br />
-            <?php echo $form->passwordField($model, 'new_password', ['size' => 40, 'maxlength' => 255]); ?>
-            <?php echo $form->error($model, 'new_password'); ?>
+
+        <div class="row<?= $model->hasErrors('new_password') ? ' error' : '' ?>">
+            <?= Html::activeLabel($model, 'new_password') ?><br />
+            <?= Html::activePasswordInput($model, 'new_password', ['size' => 40, 'maxlength' => 255]) ?><br />
+            <?= Html::error($model, 'new_password', ['class' => 'errorMessage']) ?>
         </div>
-        <div class="row">
-            <?php echo $form->labelEx($model, 'new_confirm'); ?><br />
-            <?php echo $form->passwordField($model, 'new_confirm', ['size' => 40, 'maxlength' => 255]); ?>
-            <?php echo $form->error($model, 'new_confirm'); ?>
+
+        <div class="row<?= $model->hasErrors('new_confirm') ? ' error' : '' ?>">
+            <?= Html::activeLabel($model, 'new_confirm') ?><br />
+            <?= Html::activePasswordInput($model, 'new_confirm', ['size' => 40, 'maxlength' => 255]) ?><br />
+            <?= Html::error($model, 'new_confirm', ['class' => 'errorMessage']) ?>
         </div>
 
         <hr />
+
         <div class="row buttons">
             <?php echo CHtml::submitButton('Сохранить'); ?>
         </div>
     </div>
 
-    <?php $this->endWidget(); ?>
+    </form>
 
 </div><!-- form -->
 
