5d2c6286deedcb0b42f75ded750b4561deb44b01
ElisDN <mail@elisdn.ru>
2019-10-26 22:23:30 +0300

    Removed unused code

--- src/modules/ulogin/widgets/views/uloginWidget.php
+++ src/modules/ulogin/widgets/views/uloginWidget.php
@@ -8,9 +8,4 @@
 ?>
 <?php if (Yii::app()->user->isGuest) : ?>
     <div id="uLogin" data-ulogin="display=<?php echo $display ?>;fields=<?php echo $fields ?>;providers=<?php echo $providers ?>;hidden=<?php echo $hidden ?>;redirect_uri=<?php echo urlencode($redirect) ?>"></div>
-<?php else : ?>
-    <?php
-    $anchor = 'Выйти (' . Yii::app()->user->getName() . ')';
-    echo CHtml::link(CHtml::encode($anchor), []);
-    ?>
-<?php endif ?>
+<?php endif; ?>
