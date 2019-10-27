c4a61f817663b50bf460d40a31e1bb8eed4df0c5
ElisDN <mail@elisdn.ru>
2019-10-27 20:18:01 +0300

    Fixed search

--- src/modules/search/models/Search.php
+++ src/modules/search/models/Search.php
@@ -23,7 +23,13 @@ class Search extends ActiveRecord
     public function getMaterial()
     {
         if ($this->cachedMaterial === null) {
-            $this->cachedMaterial = CActiveRecord::model($this->material_class)->findByPk($this->material_id);
+            $class = $this->material_class;
+            if (is_subclass_of($class, ActiveRecord::class)) {
+                /** @var ActiveRecord $class */
+                $this->cachedMaterial = $class::findOne($this->material_id);
+            } else {
+                $this->cachedMaterial = CActiveRecord::model($this->material_class)->findByPk($this->material_id);
+            }
         }
 
         return $this->cachedMaterial;
