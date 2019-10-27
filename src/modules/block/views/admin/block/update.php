53dd6f69830e3bf70a1a1c828fecc820db1a7cdc
ElisDN <mail@elisdn.ru>
2019-10-27 20:14:51 +0300

    Fixed block shortcode example

--- src/modules/block/views/admin/block/update.php
+++ src/modules/block/views/admin/block/update.php
@@ -1,5 +1,6 @@
 <?php
 use app\modules\block\models\Block;
+use yii\helpers\Html;
 
 /** @var $model Block */
 
@@ -17,7 +18,7 @@ $this->params['admin'][] = ['label' => 'Просмотр', 'url' => ['view', 'id
 <h1>Редактирование блока</h1>
 
 <p class="note">Код для вставки этого блока на страницу:
-    <b><?= $this->InlineWidgetsBehavior->startBlock ?>block|id=<?= CHtml::encode($model->alias) ?><?= $this->InlineWidgetsBehavior->endBlock ?></b>
+    <b>[{widget:block|id=<?= Html::encode($model->alias) ?>}]</b>
 </p>
 
 <?= $this->render('_form', ['model' => $model]); ?>
