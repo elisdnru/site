977d72338baf240d675c76db46af8d3c7632dee6
ElisDN <mail@elisdn.ru>
2019-10-28 17:07:20 +0300

    Improved admin access control

--- src/components/AdminController.php
+++ src/components/AdminController.php
@@ -3,6 +3,7 @@
 namespace app\components;
 
 use app\components\module\ModuleAdminFilter;
+use app\modules\user\models\Access;
 use CHttpException;
 use Yii;
 use yii\caching\TagDependency;
@@ -21,13 +22,13 @@ abstract class AdminController extends Controller
 
     public function beforeAction($action): bool
     {
+        if (!Yii::$app->user->can(Access::CONTROL)) {
+            throw new CHttpException(403, 'Отказано в доступе');
+        }
+
         $tag = $this->getModule()->getId();
         Yii::app()->cache->clear($tag);
         TagDependency::invalidate(Yii::$app->cache, $tag);
-
-        if (Yii::$app->user->getIsGuest()) {
-            throw new CHttpException(403, 'Отказано в доступе');
-        }
 
         return parent::beforeAction($action);
     }
