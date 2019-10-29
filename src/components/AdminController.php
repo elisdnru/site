82ab8159b1bdfd4f5b8b34fdb8e2f17d3bc9cc5f
ElisDN <mail@elisdn.ru>
2019-10-29 13:31:47 +0300

    Refactored module admin access

--- src/components/AdminController.php
+++ src/components/AdminController.php
@@ -2,9 +2,6 @@
 
 namespace app\components;
 
-use app\components\module\ModuleAdminFilter;
-use app\modules\user\models\Access;
-use CHttpException;
 use Yii;
 use yii\caching\TagDependency;
 
@@ -12,20 +9,28 @@ abstract class AdminController extends Controller
 {
     public $layout = '@app/views/layouts/admin';
 
+    public function accessRules(): array
+    {
+        return [
+            ['allow',
+                'roles' => ['module_' . $this->module->id],
+            ],
+            ['deny',
+                'users' => ['*'],
+            ],
+        ];
+    }
+
     public function filters(): array
     {
-        return array_merge(parent::filters(), [
-            [ModuleAdminFilter::class],
+        return [
+            'accessControl',
             'postOnly + delete, toggle',
-        ]);
+        ];
     }
 
     public function beforeAction($action): bool
     {
-        if (!Yii::$app->user->can(Access::CONTROL)) {
-            throw new CHttpException(403, 'Отказано в доступе');
-        }
-
         $tag = $this->getModule()->getId();
         Yii::app()->cache->clear($tag);
         TagDependency::invalidate(Yii::$app->cache, $tag);
