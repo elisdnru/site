e202a98738e32513d4c22e0d6b89259afbabdacb
ElisDN <mail@elisdn.ru>
2019-11-09 15:49:28 +0300

    Fixed comment nullables

---
+++ src/modules/comment/migrations/m191109_153821_add_nullable.php
@@ -0,0 +1,24 @@
+<?php
+// phpcs:disable
+// PSR1.Classes.ClassDeclaration.MissingNamespace
+
+use yii\db\Migration;
+
+class m191109_153821_add_nullable extends Migration
+{
+    public function safeUp()
+    {
+        $this->alterColumn('comments', 'name', 'varchar(255) DEFAULT NULL');
+        $this->alterColumn('comments', 'email', 'varchar(255) DEFAULT NULL');
+        $this->alterColumn('comments', 'site', 'varchar(255) DEFAULT NULL');
+
+        $this->update('comments', ['name' => null], ['name' => '']);
+        $this->update('comments', ['email' => null], ['email' => '']);
+        $this->update('comments', ['site' => null], ['site' => '']);
+    }
+
+    public function safeDown(): bool
+    {
+        return false;
+    }
+}
