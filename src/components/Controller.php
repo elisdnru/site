794586e40b88ec5880944d487a7391811c8c7bd0
ElisDN <mail@elisdn.ru>
2020-02-13 17:39:19 +0300

    Removed old route emulation

--- src/components/Controller.php
+++ src/components/Controller.php
@@ -2,18 +2,8 @@
 
 namespace app\components;
 
-use CController;
-use CInlineAction;
-use CWebModule;
-use Yii;
 use yii\web\Controller as Base;
 
 abstract class Controller extends Base
 {
-    public function beforeAction($action): bool
-    {
-        Yii::app()->controller = new CController($this->id, new CWebModule($this->module->id, Yii::app()));
-        Yii::app()->controller->action = new CInlineAction(Yii::app()->controller, $this->action->id);
-        return parent::beforeAction($action);
-    }
 }
