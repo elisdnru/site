912ac3f84158688cacccbd64e3e366de9fa4d96b
ElisDN <mail@elisdn.ru>
2020-02-13 17:44:03 +0300

    Removed unused files

--- src/widgets/views/BreadCrumbs.php
+++
@@ -1,6 +0,0 @@
-<?php
-/** @var $links array */
-?>
-<?php if (count($links)) : ?>
-    <aside><?php Yii::app()->controller->widget('zii.widgets.CBreadcrumbs', ['links' => $links]); ?></aside>
-<?php endif; ?>
