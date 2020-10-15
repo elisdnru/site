c282355d501e65b54cd80610594a13f0b9bb300d
ElisDN <mail@elisdn.ru>
2020-10-15 16:24:33 +0300

    Extracted XML parameters

--- src/components/module/sitemap/Item.php
+++ src/components/module/sitemap/Item.php
@@ -12,19 +12,9 @@ use Webmozart\Assert\Assert;
  */
 class Item
 {
-    public const ALWAYS = 'always';
-    public const HOURLY = 'hourly';
-    public const DAILY = 'daily';
-    public const WEEKLY = 'weekly';
-    public const MONTHLY = 'monthly';
-    public const YEARLY = 'yearly';
-    public const NEVER = 'never';
-
     public string $url;
-    public string $label;
-    public string $changeFreq;
-    public float $priority;
-    public ?DateTimeImmutable $lastMod = null;
+    public ?string $label;
+    public ?Xml $xml;
     /**
      * @var Item[]
      */
@@ -33,37 +23,17 @@ class Item
     /**
      * Item constructor.
      * @param string $url
-     * @param string $label
-     * @param string $changeFreq
-     * @param float $priority
-     * @param DateTimeImmutable|null $lastMod
+     * @param string|null $label
+     * @param Xml|null $xml
      * @param Item[] $children
      */
-    public function __construct(
-        string $url,
-        string $label,
-        string $changeFreq,
-        float $priority,
-        ?DateTimeImmutable $lastMod,
-        array $children
-    ) {
+    public function __construct(string $url, ?string $label, ?Xml $xml, array $children)
+    {
         Assert::notEmpty($url);
-
-        Assert::inArray($changeFreq, [
-            self::ALWAYS,
-            self::HOURLY,
-            self::DAILY,
-            self::WEEKLY,
-            self::MONTHLY,
-            self::YEARLY,
-            self::NEVER,
-        ]);
 
         $this->label = $label;
         $this->url = $url;
-        $this->changeFreq = $changeFreq;
-        $this->priority = $priority;
-        $this->lastMod = $lastMod;
+        $this->xml = $xml;
         $this->children = $children;
     }
 }
---
+++ src/components/module/sitemap/Xml.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\components\module\sitemap;
+
+use DateTimeImmutable;
+use Webmozart\Assert\Assert;
+
+class Xml
+{
+    public const ALWAYS = 'always';
+    public const HOURLY = 'hourly';
+    public const DAILY = 'daily';
+    public const WEEKLY = 'weekly';
+    public const MONTHLY = 'monthly';
+    public const YEARLY = 'yearly';
+    public const NEVER = 'never';
+
+    public string $changeFreq;
+    public float $priority;
+    public ?DateTimeImmutable $lastMod;
+
+    public function __construct(string $changeFreq, float $priority, ?DateTimeImmutable $lastMod)
+    {
+        Assert::notEmpty($changeFreq);
+
+        Assert::inArray($changeFreq, [
+            self::ALWAYS,
+            self::HOURLY,
+            self::DAILY,
+            self::WEEKLY,
+            self::MONTHLY,
+            self::YEARLY,
+            self::NEVER,
+        ]);
+
+        $this->changeFreq = $changeFreq;
+        $this->priority = $priority;
+        $this->lastMod = $lastMod;
+    }
+}
