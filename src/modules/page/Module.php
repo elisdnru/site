b219f9d937b9815ddeb127023e086cf737cf0a13
ElisDN <mail@elisdn.ru>
2020-10-15 18:44:11 +0300

    Removed unused code

--- src/modules/page/Module.php
+++ src/modules/page/Module.php
@@ -42,8 +42,6 @@ class Module extends Base implements UrlProvider, SitemapProvider
     public static function rules(): array
     {
         return [
-            'page/page/show' => 'site/error',
-            'page/page' => 'site/error',
             ['class' => components\PageUrlRule::class, 'cache' => 3600 * 24],
         ];
     }
