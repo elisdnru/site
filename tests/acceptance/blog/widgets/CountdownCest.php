38c563fa3c78930fe0ab3acc2c0a9eb8033d1f6e
ElisDN <mail@elisdn.ru>
2020-10-31 12:16:37 +0300

    Added inline widgets tests

---
+++ tests/acceptance/blog/widgets/CountdownCest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace tests\acceptance\blog\widgets;
+
+use tests\AcceptanceTester;
+use tests\fixtures\blog\CategoryFixture;
+use tests\fixtures\blog\CommentFixture;
+use tests\fixtures\blog\GroupFixture;
+use tests\fixtures\blog\PostFixture;
+use tests\fixtures\user\UserFixture;
+
+class CountdownCest
+{
+    // phpcs:disable
+    // PSR2.Method Declarations.Underscore
+    public function _before(AcceptanceTester $I): void
+    {
+        $I->haveFixtures([
+            'category' => CategoryFixture::class,
+            'group' => GroupFixture::class,
+            'user' => UserFixture::class,
+            'post' => PostFixture::class,
+        ]);
+    }
+
+    public function show(AcceptanceTester $I): void
+    {
+        $I->amOnPage('blog/12/post-with-countdown');
+        $I->seeResponseCodeIs(200);
+        $I->seeElement('.entry .countdown');
+    }
+}
---
+++ tests/acceptance/blog/widgets/SubscribeNewsCest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace tests\acceptance\blog\widgets;
+
+use tests\AcceptanceTester;
+use tests\fixtures\blog\CategoryFixture;
+use tests\fixtures\blog\CommentFixture;
+use tests\fixtures\blog\GroupFixture;
+use tests\fixtures\blog\PostFixture;
+use tests\fixtures\user\UserFixture;
+
+class SubscribeNewsCest
+{
+    // phpcs:disable
+    // PSR2.Method Declarations.Underscore
+    public function _before(AcceptanceTester $I): void
+    {
+        $I->haveFixtures([
+            'category' => CategoryFixture::class,
+            'group' => GroupFixture::class,
+            'user' => UserFixture::class,
+            'post' => PostFixture::class,
+        ]);
+    }
+
+    public function show(AcceptanceTester $I): void
+    {
+        $I->amOnPage('blog/10/post-with-subscribe-news');
+        $I->seeResponseCodeIs(200);
+        $I->seeElement('.entry .subscribe-after-post');
+    }
+}
