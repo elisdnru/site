61bd06226bdab74c983fc6c7a3badca43beb0450
ElisDN <mail@elisdn.ru>
2020-10-31 11:22:06 +0300

    Added del avatar test

--- tests/acceptance/profile/EditCest.php
+++ tests/acceptance/profile/EditCest.php
@@ -4,6 +4,7 @@ namespace tests\acceptance\profile;
 
 use tests\AcceptanceTester;
 use tests\fixtures\user\UserFixture;
+use yii\helpers\FileHelper;
 
 class EditCest
 {
@@ -14,6 +15,10 @@ class EditCest
         $I->haveFixtures([
             'user' => UserFixture::class,
         ]);
+
+        $dir = __DIR__ . '/../../../public/upload/images/users/avatars';
+        FileHelper::createDirectory($dir);
+        copy(__DIR__ . '/../../_data/files/avatar.png', $dir . '/160b82352f2c6025f74398703828a2ff.png');
     }
 
     public function guest(AcceptanceTester $I): void
@@ -42,6 +47,25 @@ class EditCest
         $I->see('Bond James', 'h3');
         $I->seeLink('http://app.test', 'http://app.test');
         $I->seeInSource('<img src="/upload/images/users/avatars/');
+    }
+
+    public function delAvatar(AcceptanceTester $I): void
+    {
+        $I->amLoggedInBy(6);
+        $I->amOnPage('profile/edit');
+        $I->see('Редактировать профиль', '.portlet-title');
+        $I->seeInSource('<img src="/upload/images/users/avatars/');
+
+        $I->seeElement('#profile-form');
+
+        $I->fillField('ProfileForm[del_avatar]', '1');
+
+        $I->click('Сохранить', '#profile-form');
+
+        $I->see('Профиль сохранён.', '.flash-success');
+
+        $I->see('Avatar User', 'h3');
+        $I->dontSeeInSource('<img src="/upload/images/users/avatars/');
     }
 
     public function validation(AcceptanceTester $I): void
