1bd5991c8147a3bec251c386b7c56acd397613b7
ElisDN <mail@elisdn.ru>
2020-12-13 15:29:30 +0300

    Added view stub

---
+++ psalm/yii/base/View.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace yii\base;
+
+class View extends Component implements DynamicContentAwareInterface
+{
+    /**
+     * @psalm-var array{
+     *     breadcrumbs: array,
+     *     admin: array,
+     * }
+     */
+    public $params = [];
+    /**
+     * @var ViewContextInterface|Controller
+     */
+    public $context;
+
+    public function getDynamicPlaceholders()
+    {
+        // TODO: Implement getDynamicPlaceholders() method.
+    }
+
+    public function setDynamicPlaceholders($placeholders)
+    {
+        // TODO: Implement setDynamicPlaceholders() method.
+    }
+
+    public function addDynamicPlaceholder($name, $statements)
+    {
+        // TODO: Implement addDynamicPlaceholder() method.
+    }
+}
