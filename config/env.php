86625ee154baff31bcb2794b433b1fa33e8ea688
Dmitry Eliseev <mail@elisdn.ru>
2021-01-15 04:22:38 +0300

    Added own env test constant

--- config/env.php
+++ config/env.php
@@ -2,4 +2,5 @@
 
 defined('YII_DEBUG') or define('YII_DEBUG', (bool)getenv('APP_DEBUG'));
 defined('YII_ENV') or define('YII_ENV', getenv('APP_ENV'));
+defined('YII_ENV_TEST') or define('YII_ENV_TEST', getenv('APP_ENV') === 'test');
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
