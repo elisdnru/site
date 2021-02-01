551a31653a6432496f807108d1b2fbc5e342528b
ElisDN <mail@elisdn.ru>
2021-02-01 08:33:51 +0300

    Improved test

--- tests/acceptance/PartnerCest.php
+++ tests/acceptance/PartnerCest.php
@@ -12,5 +12,6 @@ class PartnerCest
         $I->seeResponseCodeIs(200);
         $I->see('Парнёрская программа', 'h1');
         $I->see('Парнёрская программа', 'title');
+        $I->seeLink('Подключиться к программе', 'https://products.elisdn.ru/join/');
     }
 }
