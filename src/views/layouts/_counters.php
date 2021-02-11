55d455d42eb4a93bbb34f15c56321a4baa35e60a
ElisDN <mail@elisdn.ru>
2021-02-11 14:40:02 +0300

    Removed LI counter

--- src/views/layouts/_counters.php
+++ src/views/layouts/_counters.php
@@ -7,20 +7,6 @@ use yii\web\View;
  */
 ?>
 
-<div>
-    <script>
-    document.write('<a href=\'http://www.liveinternet.ru/click\' ' +
-        'target=_blank><img src=\'//counter.yadro.ru/hit?t16.3;r' +
-        escape(document.referrer) + ((typeof (screen) == 'undefined') ? '' :
-            ';s' + screen.width + '*' + screen.height + '*' + (screen.colorDepth ?
-            screen.colorDepth : screen.pixelDepth)) + ';u' + escape(document.URL) +
-        ';' + Math.random() +
-        '\' alt=\'\' title=\'LiveInternet: показано число просмотров за 24' +
-        ' часа, посетителей за 24 часа и за сегодня\' ' +
-        'border=\'0\' width=\'88\' height=\'31\'><\/a>');
-    </script>
-</div>
-
 <script>
 <?php ob_start(); ?>
 (function (m, e, t, r, i, k, a) {
