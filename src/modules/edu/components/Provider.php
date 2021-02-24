47bc12c800163318fab7e2b2a7875ec20fdedf28
ElisDN <mail@elisdn.ru>
2021-02-24 11:29:34 +0300

    Removed unused definition

--- src/modules/edu/components/Provider.php
+++ src/modules/edu/components/Provider.php
@@ -8,24 +8,18 @@ use app\modules\edu\components\api\Api;
 use app\modules\edu\components\api\client\Cached;
 use app\modules\edu\components\api\client\Muted;
 use Http\Client\Curl\Client;
-use Psr\Http\Client\ClientInterface;
 use Psr\SimpleCache\CacheInterface;
 use Yii;
 use yii\base\BootstrapInterface;
 use yii\di\Container;
 use Laminas\Diactoros\RequestFactory;
 use Laminas\Diactoros\ResponseFactory;
-use Laminas\Diactoros\StreamFactory;
 
 class Provider implements BootstrapInterface
 {
     public function bootstrap($app): void
     {
         $container = Yii::$container;
-
-        $container->setSingleton(ClientInterface::class, static function () {
-            return new Client(new ResponseFactory(), new StreamFactory());
-        });
 
         $container->setSingleton(Api::class, static function (Container $container) use ($app) {
             /** @var CacheInterface $cache */
