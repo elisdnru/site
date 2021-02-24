2249325643711b8c5cfb4215983620bbe7cfcf1a
ElisDN <mail@elisdn.ru>
2021-02-24 12:41:45 +0300

    Migrated to container

--- src/components/psr/SimpleCacheProvider.php
+++ src/components/psr/SimpleCacheProvider.php
@@ -7,6 +7,8 @@ namespace app\components\psr;
 use Psr\SimpleCache\CacheInterface;
 use Yii;
 use yii\base\BootstrapInterface;
+use yii\caching\CacheInterface as YiiCacheInterface;
+use yii\di\Container;
 
 class SimpleCacheProvider implements BootstrapInterface
 {
@@ -14,8 +16,10 @@ class SimpleCacheProvider implements BootstrapInterface
     {
         $container = Yii::$container;
 
-        $container->setSingleton(CacheInterface::class, static function () use ($app) {
-            return new SimpleCache($app->cache);
+        $container->setSingleton(CacheInterface::class, static function (Container $container) {
+            return new SimpleCache(
+                $container->get(YiiCacheInterface::class)
+            );
         });
     }
 }
