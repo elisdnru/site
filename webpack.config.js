14a56aca89beb3aab89b1ccb50ee8865a6de71ed
ElisDN <mail@elisdn.ru>
2021-02-26 14:38:59 +0300

    Installed Babel

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.(png|jpg|gif)$/,
         use: ['file-loader']
       },
