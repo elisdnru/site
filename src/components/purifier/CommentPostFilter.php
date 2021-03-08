6f79695ed43aab0366a26c43a6d3c0d6623c781d
ElisDN <mail@elisdn.ru>
2021-03-08 06:18:06 +0300

    Fixed type

--- src/components/purifier/CommentPostFilter.php
+++ src/components/purifier/CommentPostFilter.php
@@ -6,8 +6,12 @@ class CommentPostFilter
 {
     public static function fixMarkup(?string $text): ?string
     {
+        if ($text === null) {
+            return null;
+        }
+
         return preg_replace_callback('@(<p>.*?</p>)@s', static function (array $matches): string {
-            return nl2br($matches[0]);
+            return nl2br((string)$matches[0]);
         }, $text);
     }
 }
