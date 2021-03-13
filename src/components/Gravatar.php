769fc458413557c0d978eef57160dae21d0eaa61
ElisDN <mail@elisdn.ru>
2021-03-13 13:45:16 +0300

    Simplified Gravatar

--- src/components/Gravatar.php
+++ src/components/Gravatar.php
@@ -4,19 +4,9 @@ namespace app\components;
 
 class Gravatar
 {
-    public const DEFAULT_GRAVATAR = '';
-    public const DEFAULT_404 = '404';
-    public const DEFAULT_MYSTERYMAN = 'mm';
-    public const DEFAULT_ABSTRACT = 'identicon';
-    public const DEFAULT_FACE = 'wavatar';
-    public const DEFAULT_MONSTER = 'monsterid';
-    public const DEFAULT_RETRO = 'retro';
-    public const DEFAULT_BLANK = 'blank';
-
-    public static function url(?string $email, int $width = 0, string $default = self::DEFAULT_ABSTRACT): string
+    public static function url(?string $email, int $width): string
     {
         $id = md5(strtolower(trim($email ?: '')));
-        $default = '?d=' . urlencode($default);
-        return 'https://www.gravatar.com/avatar/' . $id . $default . ($width ? '&s=' . $width : '');
+        return 'https://www.gravatar.com/avatar/' . $id . '?d=identicon' . ($width ? '&s=' . $width : '');
     }
 }
