2a994055162fc083923cb24de341327ce0b3cce4
Dmitry Eliseev <mail@elisdn.ru>
2021-03-15 22:53:38 +0300

    Simplified cache provider

--- src/components/psr/SimpleCacheProvider.php
+++ src/components/psr/SimpleCacheProvider.php
@@ -7,8 +7,6 @@ namespace app\components\psr;
 use Psr\SimpleCache\CacheInterface;
 use Yii;
 use yii\base\BootstrapInterface;
-use yii\caching\CacheInterface as YiiCacheInterface;
-use yii\di\Container;
 
 class SimpleCacheProvider implements BootstrapInterface
 {
@@ -16,10 +14,6 @@ class SimpleCacheProvider implements BootstrapInterface
     {
         $container = Yii::$container;
 
-        $container->setSingleton(CacheInterface::class, static function (Container $container) {
-            return new SimpleCacheAdapter(
-                $container->get(YiiCacheInterface::class)
-            );
-        });
+        $container->setSingleton(CacheInterface::class, SimpleCacheAdapter::class);
     }
 }
