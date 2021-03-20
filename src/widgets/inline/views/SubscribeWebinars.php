c6ecd3226c7068074d358036312f69847a3c2419
ElisDN <mail@elisdn.ru>
2021-03-20 13:19:28 +0300

    Fixed codestyle

--- src/widgets/inline/views/SubscribeWebinars.php
+++ src/widgets/inline/views/SubscribeWebinars.php
@@ -1,22 +1,22 @@
 <div class="subscribe-after-post">
-<div class="subscribe-form" style="padding-top: 4px">
-    <form method="post" action="#" data-action="//elisdn.justclick.ru/subscribe/process/?rid%5B0%5D=webinars" target="_blank">
-        <div class="row">
-            <input type="text" name="lead_name" placeholder="Ваше имя" required />
-        </div>
-        <div class="row">
-            <input type="email" name="lead_email" placeholder="Ваш Email" required />
-        </div>
-        <div class="row button">
-            <button type="submit">Подписаться</button>
-        </div>
-        <div class="clear"></div>
-        <div>
-            <label>
-                <input type="checkbox" required />
-                согласен с <a target="_blank" href="/privacy">политикой конфиденциальности</a>
-            </label>
-        </div>
-    </form>
-</div>
+    <div class="subscribe-form" style="padding-top: 4px">
+        <form method="post" action="#" data-action="//elisdn.justclick.ru/subscribe/process/?rid%5B0%5D=webinars" target="_blank">
+            <div class="row">
+                <input type="text" name="lead_name" placeholder="Ваше имя" required />
+            </div>
+            <div class="row">
+                <input type="email" name="lead_email" placeholder="Ваш Email" required />
+            </div>
+            <div class="row button">
+                <button type="submit">Подписаться</button>
+            </div>
+            <div class="clear"></div>
+            <div>
+                <label>
+                    <input type="checkbox" required />
+                    согласен с <a target="_blank" href="/privacy">политикой конфиденциальности</a>
+                </label>
+            </div>
+        </form>
+    </div>
 </div>
