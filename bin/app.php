5c7ae3daeca002e93fff5266ca783656535a6bb2
ElisDN <mail@elisdn.ru>
2021-06-19 18:51:34 +0300

    Removed test app

--- bin/app.php
+++ bin/app.php
@@ -12,6 +12,11 @@ if (file_exists(__DIR__ . '/../.env')) {
 require_once __DIR__ . '/../config/env.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-$config = require __DIR__ . '/../config/console.php';
+if (getenv('APP_ENV') === 'test') {
+    $config = require __DIR__ . '/../config/console-test.php';
+} else {
+    $config = require __DIR__ . '/../config/console.php';
+}
+
 $app = new yii\console\Application($config);
 $app->run();
