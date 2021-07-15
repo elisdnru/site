6b4a21dae236a55ebfa727e4a5c78526b02a60e6
ElisDN <mail@elisdn.ru>
2021-07-15 23:15:55 +0300

    Removed unused test configs

--- bin/app.php
+++ bin/app.php
@@ -6,11 +6,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/env.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-if (getenv('APP_ENV') === 'test') {
-    $config = require __DIR__ . '/../config/console-test.php';
-} else {
-    $config = require __DIR__ . '/../config/console.php';
-}
+$config = require __DIR__ . '/../config/console.php';
 
 $app = new yii\console\Application($config);
 $app->run();
--- public/index.php
+++ public/index.php
@@ -11,11 +11,7 @@ if (getenv('SENTRY_DSN')) {
 require_once __DIR__ . '/../config/env.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-if (getenv('APP_ENV') === 'test') {
-    $config = require __DIR__ . '/../config/web-test.php';
-} else {
-    $config = require __DIR__ . '/../config/web.php';
-}
+$config = require __DIR__ . '/../config/web.php';
 
 $app = new yii\web\Application($config);
 $app->run();
