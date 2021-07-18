9f6c5348769b86e11faa80c8082ef42824f1b802
ElisDN <mail@elisdn.ru>
2021-07-18 18:45:46 +0300

    Updated Google Analytics

--- src/widgets/views/Counters.php
+++ src/widgets/views/Counters.php
@@ -7,35 +7,30 @@ use yii\web\View;
  */
 ?>
 
-<script>
-(function (m, e, t, r, i, k, a) {
-    m[i] = m[i] || function () {(m[i].a = m[i].a || []).push(arguments)}
-    m[i].l = 1 * new Date()
-    k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
-})(window, document, 'script', 'https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js', 'ym');
-ym(18406216, 'init', {
-    clickmap: true,
-    trackLinks: true,
-    accurateTrackBounce: true,
-    webvisor: true
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js", "ym");
+
+ym(18406216, "init", {
+    clickmap:true,
+    trackLinks:true,
+    accurateTrackBounce:true,
+    webvisor:true
 });
 </script>
-<noscript>
-    <div><img src="https://mc.yandex.ru/watch/18406216" style="position:absolute; left:-9999px;" alt=""></div>
-</noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/18406216" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-36477750-1"></script>
 <script>
-var _gaq = _gaq || []
-_gaq.push(['_setAccount', 'UA-3647' + '7750-1'])
-_gaq.push(['_trackPageview']);
-(function () {
-    var ga = document.createElement('script')
-    ga.type = 'text/javascript'
-    ga.async = true
-    ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'
-    var s = document.getElementsByTagName('script')[0]
-    s.parentNode.insertBefore(ga, s)
-})();
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-3647' + '7750-1');
 </script>
 
 <script>
