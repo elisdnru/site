47863b291c9ded29fc8bf8964414f27a776c0b0a
ElisDN <mail@elisdn.ru>
2021-07-20 14:13:32 +0300

    Update CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -35,10 +35,6 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpg|gif)$/,
-        use: ['file-loader'],
-      },
-      {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
