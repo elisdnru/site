9acd67560bd235c27cb8b823e362e401f9af4377
ElisDN <mail@elisdn.ru>
2021-07-25 14:27:07 +0300

    Add env requirenments

--- docker/common/files-backup/backup.sh
+++ docker/common/files-backup/backup.sh
@@ -7,17 +7,18 @@ if [ -f "$AWS_SECRET_ACCESS_KEY_FILE" ]; then
   AWS_SECRET_ACCESS_KEY="$(cat "$AWS_SECRET_ACCESS_KEY_FILE")"
 fi
 
-BACKUP_FILE="${BACKUP_NAME}_$(date +%Y-%m-%d_%H-%M).tar"
+BACKUP_FILE="${BACKUP_NAME:?}_$(date +%Y-%m-%d_%H-%M).tar"
 
 echo "Archive $BACKUP_FILE"
 
-tar -cpf "$BACKUP_FILE" "$TARGET"
+tar -cpf "$BACKUP_FILE" "${TARGET:?}"
 
 echo "Upload to S3"
 
-export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
-export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:?}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:?}"
+export AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:?}"
 
-aws --endpoint-url="$S3_ENDPOINT" s3 cp "$BACKUP_FILE" "s3://$S3_BUCKET/$BACKUP_FILE"
+aws --endpoint-url="${S3_ENDPOINT:?}" s3 cp "$BACKUP_FILE" "s3://${S3_BUCKET:?}/$BACKUP_FILE"
 
 unlink "$BACKUP_FILE"
--- docker/common/mysql-backup/backup.sh
+++ docker/common/mysql-backup/backup.sh
@@ -11,17 +11,19 @@ if [ -f "$AWS_SECRET_ACCESS_KEY_FILE" ]; then
   AWS_SECRET_ACCESS_KEY="$(cat "$AWS_SECRET_ACCESS_KEY_FILE")"
 fi
 
-BACKUP_FILE="${BACKUP_NAME}_$(date +%Y-%m-%d_%H-%M).sql.gz"
+BACKUP_FILE="${BACKUP_NAME:?}_$(date +%Y-%m-%d_%H-%M).sql.gz"
 
 echo "Dump $BACKUP_FILE"
 
-mysqldump --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --single-transaction "$MYSQL_DB" | gzip -9 > $BACKUP_FILE
+mysqldump --host="${MYSQL_HOST:?}" --user="${MYSQL_USER:?}" --password="${MYSQL_PASSWORD:?}" \
+  --single-transaction "${MYSQL_DB:?}" | gzip -9 > $BACKUP_FILE
 
 echo "Upload to S3"
 
-export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
-export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:?}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:?}"
+export AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:?}"
 
-aws --endpoint-url="$S3_ENDPOINT" s3 cp "$BACKUP_FILE" "s3://$S3_BUCKET/$BACKUP_FILE"
+aws --endpoint-url="${S3_ENDPOINT:?}" s3 cp "$BACKUP_FILE" "s3://${S3_BUCKET:?}/$BACKUP_FILE"
 
 unlink "$BACKUP_FILE"
