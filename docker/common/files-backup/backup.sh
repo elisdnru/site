3032ef6ac1c1d0dc4be884a1e5001f976ae9c40d
ElisDN <mail@elisdn.ru>
2021-07-25 13:41:23 +0300

    Improve backup scripts

--- docker/common/files-backup/backup.sh
+++ docker/common/files-backup/backup.sh
@@ -3,24 +3,21 @@
 set -o errexit
 set -o pipefail
 
-if [ -f "$AWS_ACCESS_KEY_ID_FILE" ]; then
-  AWS_ACCESS_KEY_ID="$(cat $AWS_ACCESS_KEY_ID_FILE)"
-fi
-
 if [ -f "$AWS_SECRET_ACCESS_KEY_FILE" ]; then
-  AWS_SECRET_ACCESS_KEY="$(cat $AWS_SECRET_ACCESS_KEY_FILE)"
+  AWS_SECRET_ACCESS_KEY=$(cat "$AWS_SECRET_ACCESS_KEY_FILE")
 fi
 
-BACKUP_FILE=site_${BACKUP_NAME}_$(date +%Y-%m-%d_%H-%M).tar
+BACKUP_FILE="${BACKUP_NAME}_$(date +%Y-%m-%d_%H-%M).tar"
 
 echo "Archive $BACKUP_FILE"
 
-tar -cpf /tmp/$BACKUP_FILE $TARGET
+tar -cpf "$BACKUP_FILE" "$TARGET"
 
 echo "Upload to S3"
 
 export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
-aws --endpoint-url=$S3_ENDPOINT s3 cp /tmp/$BACKUP_FILE s3://$S3_BUCKET/$BACKUP_FILE
 
-unlink /tmp/$BACKUP_FILE
+aws --endpoint-url="$S3_ENDPOINT" s3 cp "$BACKUP_FILE" "s3://$S3_BUCKET/$BACKUP_FILE"
+
+unlink "$BACKUP_FILE"
--- docker/common/mysql-backup/backup.sh
+++ docker/common/mysql-backup/backup.sh
@@ -4,22 +4,24 @@ set -o errexit
 set -o pipefail
 
 if [ -f "$MYSQL_PASSWORD_FILE" ]; then
-  MYSQL_PASSWORD="$(cat $MYSQL_PASSWORD_FILE)"
+  MYSQL_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
 fi
 
 if [ -f "$AWS_SECRET_ACCESS_KEY_FILE" ]; then
-  AWS_SECRET_ACCESS_KEY="$(cat $AWS_SECRET_ACCESS_KEY_FILE)"
+  AWS_SECRET_ACCESS_KEY=$(cat "$AWS_SECRET_ACCESS_KEY_FILE")
 fi
 
-DUMP_FILE=site_mysql_$(date +%Y-%m-%d_%H-%M).sql.gz
+BACKUP_FILE="${BACKUP_NAME}_$(date +%Y-%m-%d_%H-%M).sql.gz"
 
-echo "Dump $DUMP_FILE"
+echo "Dump $BACKUP_FILE"
 
-mysqldump --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD --single-transaction $MYSQL_DB | gzip -9 > $DUMP_FILE
+mysqldump --host="$MYSQL_HOST" --user="$MYSQL_USER" --password="$MYSQL_PASSWORD" --single-transaction "$MYSQL_DB" | gzip -9 > $BACKUP_FILE
 
 echo "Upload to S3"
 
+export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
-aws --endpoint-url=$S3_ENDPOINT s3 cp $DUMP_FILE s3://$S3_BUCKET/$DUMP_FILE
 
-unlink $DUMP_FILE
+aws --endpoint-url="$S3_ENDPOINT" s3 cp "$BACKUP_FILE" "s3://$S3_BUCKET/$BACKUP_FILE"
+
+unlink "$BACKUP_FILE"
