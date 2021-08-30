218c593445e50fae3fae99814c4625b41f37ff7c
ElisDN <mail@elisdn.ru>
2021-08-30 15:49:36 +0300

    Improve theme posts style

--- src/modules/blog/widgets/views/ThemePosts.php
+++ src/modules/blog/widgets/views/ThemePosts.php
@@ -7,15 +7,13 @@ use app\modules\blog\models\Post;
  * @var int $current
  */
 ?>
-<br />
-<h4>Материалы по теме:</h4>
-<ul style="list-style: none; margin: 0">
+<div class="block-title">Материалы по теме</div>
+<ul style="list-style: none; margin-left: 0">
     <?php foreach ($posts as $item) : ?>
         <?php if ($item->id !== $current) : ?>
-            <li>&raquo; <a href="<?= $item->getUrl(); ?>"><?= $item->title; ?></a></li>
+            <li><a href="<?= $item->getUrl(); ?>"><?= $item->title; ?></a></li>
         <?php else : ?>
-            <li>&raquo; <?= $item->title; ?></li>
+            <li><?= $item->title; ?></li>
         <?php endif; ?>
     <?php endforeach; ?>
 </ul>
-
