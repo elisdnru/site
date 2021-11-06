33607c73e12f0b46e0643a1a5e9b871346fa0465
ElisDN <mail@elisdn.ru>
2021-11-06 10:43:59 +0300

    Update deps

--- src/components/SimpleCacheAdapter.php
+++ src/components/SimpleCacheAdapter.php
@@ -18,19 +18,19 @@ final class SimpleCacheAdapter implements CacheInterface
         $this->cache = $cache;
     }
 
-    public function get($key, $default = null)
+    public function get(string $key, mixed $default = null): mixed
     {
         /** @var false|mixed $result */
         $result = $this->cache->get($key);
         return $result !== false ? $result : $default;
     }
 
-    public function set($key, $value, $ttl = null): bool
+    public function set(string $key, mixed $value, null|int|DateInterval $ttl = null): bool
     {
         return $this->cache->set($key, $value, self::toSeconds($ttl));
     }
 
-    public function delete($key): bool
+    public function delete(string $key): bool
     {
         return $this->cache->delete($key);
     }
@@ -40,28 +40,47 @@ final class SimpleCacheAdapter implements CacheInterface
         return $this->cache->flush();
     }
 
-    public function getMultiple($keys, $default = null): array
+    public function getMultiple(iterable $keys, mixed $default = null): iterable
     {
-        /** @var string[] $keys */
-        return $this->cache->multiGet($keys);
+        $strings = [];
+        foreach ($keys as $key) {
+            $strings[] = $key;
+        }
+
+        /**
+         * @var iterable<string, mixed>
+         */
+        return $this->cache->multiGet($strings);
     }
 
-    public function setMultiple($values, $ttl = null): bool
+    public function setMultiple(iterable $values, null|int|DateInterval $ttl = null): bool
     {
-        return $this->cache->multiSet((array)$values, self::toSeconds($ttl)) !== [];
+        /**
+         * @var array<string, mixed> $items
+         */
+        $items = [];
+        /**
+         * @var string $key
+         * @var mixed $value
+         */
+        foreach ($values as $key => $value) {
+            /** @psalm-suppress MixedAssignment */
+            $items[$key] = $value;
+        }
+
+        return $this->cache->multiSet($items, self::toSeconds($ttl)) !== [];
     }
 
-    public function deleteMultiple($keys): bool
+    public function deleteMultiple(iterable $keys): bool
     {
         $res = true;
-        /** @var string $key */
         foreach ($keys as $key) {
             $res = $this->cache->delete($key) || $res;
         }
         return $res;
     }
 
-    public function has($key): bool
+    public function has(string $key): bool
     {
         return $this->cache->exists($key);
     }
