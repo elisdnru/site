da4b24881d9a75da724386101eaf9ec0b9f808a5
ElisDN <mail@elisdn.ru>
2022-05-13 15:29:03 +0300

    Install Intl

--- tests/unit/components/FilenameEscaperTest.php
+++ tests/unit/components/FilenameEscaperTest.php
@@ -14,6 +14,6 @@ final class FilenameEscaperTest extends Unit
 {
     public function testEscape(): void
     {
-        self::assertEquals('_/wro-ng/ф_айл.exe', FilenameEscaper::escape('../wro-ng/ф_айл.exe'));
+        self::assertEquals('_/wro-ng/f_ajl.exe', FilenameEscaper::escape('../wro-ng/ф_айл.exe'));
     }
 }
