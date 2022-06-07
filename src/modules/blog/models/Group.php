93779624474ecb261225198fd5cb708bc63e3c17
ElisDN <mail@elisdn.ru>
2022-06-07 22:59:05 +0300

    Revert group validation

--- src/modules/blog/models/Group.php
+++ src/modules/blog/models/Group.php
@@ -30,6 +30,22 @@ final class Group extends ActiveRecord
         return (int)Post::find()->andWhere(['group_id' => $this->id])->count();
     }
 
+    public function rules(): array
+    {
+        return [
+            ['title', 'required'],
+            ['title', 'string', 'max' => 255],
+        ];
+    }
+
+    public function attributeLabels(): array
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Наименование группы',
+        ];
+    }
+
     public function behaviors(): array
     {
         return [
