207eb0df4325d0f30ea7144fe15e5576201a441a
ElisDN <mail@elisdn.ru>
2022-06-08 18:46:47 +0300

    Fix type

--- src/modules/blog/views/admin/tag/_form.php
+++ src/modules/blog/views/admin/tag/_form.php
@@ -1,14 +1,14 @@
 <?php declare(strict_types=1);
 
 use app\components\Csrf;
-use app\modules\blog\models\Tag;
+use app\modules\blog\forms\admin\TagForm;
 use yii\helpers\Html;
 use yii\web\View;
 use yii\widgets\ActiveForm;
 
 /**
  * @var View $this
- * @var Tag $model
+ * @var TagForm $model
  * @var ActiveForm $form
  */
 ?>
