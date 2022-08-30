29ac509b2e82d4b7ca4ac424febded0aaeb81c26
ElisDN <mail@elisdn.ru>
2022-08-30 20:53:59 +0300

    Change donate

--- src/modules/donate/controllers/DefaultController.php
+++ src/modules/donate/controllers/DefaultController.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace app\modules\donate\controllers;
 
 use yii\web\Controller;
+use yii\web\Response;
 
 final class DefaultController extends Controller
 {
-    public function actionIndex(): string
+    public function actionIndex(): Response
     {
-        return $this->render('index');
+        return $this->redirect('https://products.elisdn.ru/order/donate/', 302);
     }
 }
--- tests/acceptance/DonateCest.php
+++ tests/acceptance/DonateCest.php
@@ -10,10 +10,9 @@ final class DonateCest
 {
     public function home(AcceptanceTester $I): void
     {
+        $I->stopFollowingRedirects();
         $I->amOnPage('donate');
-        $I->seeResponseCodeIs(200);
-        $I->see('Поддержать проект', 'h1');
-        $I->see('Поддержать проект', 'title');
-        $I->seeInSource('<meta name="robots" content="noindex, follow">');
+        $I->seeResponseCodeIs(302);
+        $I->haveHttpHeader('Location', 'https://products.elisdn.ru/order/donate/');
     }
 }
