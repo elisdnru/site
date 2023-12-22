5457f3ea746ef28fd4f320b0f35e95471ecaa4d7
ElisDN <mail@elisdn.ru>
2023-12-22 20:22:55 +0300

    Update Redis entrypoint

--- docker/common/redis/docker-entrypoint.sh
+++ docker/common/redis/docker-entrypoint.sh
@@ -19,7 +19,15 @@ fi
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
 	find . \! -user redis -exec chown redis '{}' +
-	exec su-exec redis "$0" "$@"
+	exec gosu redis "$0" "$@"
+fi
+
+# set an appropriate umask (if one isn't set already)
+# - https://github.com/docker-library/redis/issues/305
+# - https://github.com/redis/redis/blob/bb875603fb7ff3f9d19aad906bd45d7db98d9a39/utils/systemd-redis_server.service#L37
+um="$(umask)"
+if [ "$um" = '0022' ]; then
+	umask 0077
 fi
 
 exec "$@"
