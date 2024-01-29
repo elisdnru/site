8bfdc20d08218442e75fd47f719a944dbf6e6e07
ElisDN <mail@elisdn.ru>
2024-01-29 21:47:55 +0300

    Change bar style

--- src/widgets/views/NotificationBar.php
+++ src/widgets/views/NotificationBar.php
@@ -2,11 +2,11 @@
 <div style="
     padding: 10px;
     text-align: center;
-    background: #ff2b00;
+    background: #d94b17;
     color: #fff;
     font-family: arial, sans-serif;
     font-size: 13px;
-    line-height: 1.5;
+    line-height: 1.3;
 ">
     Производится смена системы оплаты. Оформление заказов временно недоступно.
 </div>
