c0e2c099ee01ed429eabbbd66754a7ad779539d5
Dmitry Eliseev <mail@elisdn.ru>
2024-02-02 01:18:42 +0300

    Remove notification

--- src/widgets/views/NotificationBar.php
+++ src/widgets/views/NotificationBar.php
@@ -1,13 +1,1 @@
-<!--noindex-->
-<div style="
-    padding: 10px;
-    text-align: center;
-    background: #d94b17;
-    color: #fff;
-    font-family: arial, sans-serif;
-    font-size: 13px;
-    line-height: 1.3;
-">
-    Производится смена системы оплаты. Оформление заказов временно недоступно.
-</div>
-<!--/noindex-->
+
