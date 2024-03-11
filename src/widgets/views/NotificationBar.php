b81e69d958b3f99785b2f2d7a3f0b929afe8650c
ElisDN <mail@elisdn.ru>
2024-03-11 14:10:41 +0300

    Add notification

--- src/widgets/views/NotificationBar.php
+++ src/widgets/views/NotificationBar.php
@@ -1,1 +1,15 @@
-
+<!--noindex-->
+<div style="
+    padding: 6px;
+    text-align: center;
+    background: #16a100;
+    color: #fff;
+    font-family: arial, sans-serif;
+    font-size: 13px;
+    line-height: 1.3;
+">
+    Доступна оплата мастер-классов российскими и иностранными картами,
+    ЮMoney, быстрыми платежами СБП и SberPay
+    <a style="color: #fff" href="/products">&gt;&gt;&gt;</a>
+</div>
+<!--/noindex-->
